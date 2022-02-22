fb9a8eedac23fd7e6a82ae11cd82c1534fae0d3f
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-02-22 11:19:56 +0100

    endret navn på target og den blir nå en vanlig tabell

---
+++ dmx_poc/models/marts/dmx/fak_syfo_dt_dialogmote.sql
@@ -0,0 +1,24 @@
+
+WITH fak_syfo_dialogmote_s AS (
+    SELECT * FROM {{ref('fak_syfo_dialogmote_tid')}}
+),
+
+dim_varighet AS (
+    SELECT * FROM {{ref('stg_dmx_data_dim_varighet')}}
+),
+
+final AS (
+    SELECT fak_syfo_dialogmote_s.*,
+           dim_varighet.pk_dim_varighet as fk_dim_varighet
+    FROM fak_syfo_dialogmote_s
+    LEFT JOIN dim_varighet
+    ON dim_varighet.varighet_dager = trunc(fak_syfo_dialogmote_s.nyeste_dialogmote) - trunc(fak_syfo_dialogmote_s.nyeste_tilfelle_startdato)
+)
+/*
+legger inn table som target tabell
+*/
+{{ config(
+    materialized="table"
+) }}
+
+SELECt final.* FROM final
