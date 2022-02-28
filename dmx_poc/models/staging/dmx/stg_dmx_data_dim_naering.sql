eef25fd6cb1d556a3e7e0467b2f3e452ed51c652
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-02-28 12:17:08 +0100

    lagt inn dim_naering som kilde

---
+++ dmx_poc/models/staging/dmx/stg_dmx_data_dim_naering.sql
@@ -0,0 +1,10 @@
+
+with source_dim_naering as (
+    select  * from {{ source('dmx_pox_oppfolging', 'DIM_NAERING') }}
+),
+
+final as (
+    select * from source_dim_naering
+)
+
+select * from final
