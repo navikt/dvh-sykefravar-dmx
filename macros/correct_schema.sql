a7a1dabef59e44d1f585afeb19cf9c3324572496
Patrick Pedersen <patrick.pedersen@nav.no>
2022-03-22 17:21:43 +0100

    Remove unused tests and macros

--- macros/correct_schema.sql
+++
@@ -1,3 +0,0 @@
-{% macro get_correct_schema(my_schema) %}
-    {{ return(my_schema) }}
-{% endmacro %}
--- macros/test_unique_values_syk.sql
+++
@@ -1,9 +0,0 @@
-{%macro test_unique_values_syk(model, column_name) %}
-select 
-    {{column_name}} AS unique_field,
-    count(*) AS "n_records" 
-    FROM {{model}}
-where {{column_name}} is not null
-group by {{column_name}}
-having count(*) > 1
-{% endmacro %}
