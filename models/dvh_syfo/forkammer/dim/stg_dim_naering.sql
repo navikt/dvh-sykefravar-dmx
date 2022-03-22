7bf80efa2962ce7b4b35996441660d7fe61baab5
Patrick Pedersen <patrick.pedersen@nav.no>
2022-03-22 17:23:32 +0100

    Refactor style in stg_dim_naering

--- models/dvh_syfo/forkammer/dim/stg_dim_naering.sql
+++ models/dvh_syfo/forkammer/dim/stg_dim_naering.sql
@@ -1,10 +1,9 @@
-
 with source_dim_naering as (
-    select  * from {{ source('dmx_pox_oppfolging', 'DIM_NAERING') }}
+  select * from {{ source('dmx_pox_oppfolging', 'DIM_NAERING') }}
 ),
 
 final as (
-    select * from source_dim_naering
+  select * from source_dim_naering
 )
 
 select * from final
