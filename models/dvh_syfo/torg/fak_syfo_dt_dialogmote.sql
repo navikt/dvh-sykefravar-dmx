0b82b2b94a34a937f9c2d7e88792096550bbbff9
Patrick Pedersen <patrick.pedersen@nav.no>
2022-03-23 11:05:42 +0100

    Refactor style in fak_syfo_dt_dialogmote

--- models/dvh_syfo/torg/fak_syfo_dt_dialogmote.sql
+++ models/dvh_syfo/torg/fak_syfo_dt_dialogmote.sql
@@ -1,18 +1,21 @@
-
 WITH fak_syfo_dialogmote_s AS (
-    SELECT * FROM {{ref('fak_syfo_dialogmote_tid')}}
+  SELECT * FROM {{ ref('fak_syfo_dialogmote_tid') }}
 ),
 
 dim_varighet AS (
-    SELECT * FROM {{ref('stg_dim_varighet')}}
+  SELECT * FROM {{ ref('stg_dim_varighet') }}
 ),
 
 final AS (
-    SELECT fak_syfo_dialogmote_s.*,
-           dim_varighet.pk_dim_varighet as fk_dim_varighet
-    FROM fak_syfo_dialogmote_s
-    LEFT JOIN dim_varighet
-    ON dim_varighet.varighet_dager = trunc(fak_syfo_dialogmote_s.nyeste_dialogmote) - trunc(fak_syfo_dialogmote_s.nyeste_tilfelle_startdato)
+  SELECT
+    fak_syfo_dialogmote_s.*,
+    dim_varighet.pk_dim_varighet AS fk_dim_varighet
+  FROM fak_syfo_dialogmote_s
+  LEFT JOIN dim_varighet
+    ON
+      dim_varighet.varighet_dager = trunc(
+        fak_syfo_dialogmote_s.nyeste_dialogmote
+      ) - trunc(fak_syfo_dialogmote_s.nyeste_tilfelle_startdato)
 )
 /*
 legger inn table som target tabell
@@ -21,4 +24,4 @@ legger inn table som target tabell
     materialized="table"
 ) }}
 
-SELECt final.* FROM final
+SELECT final.* FROM final
