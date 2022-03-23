3b7c6fe52085e4cf44f9948ad15d5f44918d7457
Patrick Pedersen <patrick.pedersen@nav.no>
2022-03-23 11:01:22 +0100

    Refactor style in stg_dim_tid

--- models/dvh_syfo/forkammer/dim/stg_dim_tid.sql
+++ models/dvh_syfo/forkammer/dim/stg_dim_tid.sql
@@ -1,10 +1,9 @@
-
 with source_dim_tid as (
-    select  * from {{ source('dmx_pox_oppfolging', 'DIM_TID') }}
+  select * from {{ source('dmx_pox_oppfolging', 'DIM_TID') }}
 ),
 
 final as (
-    select * from source_dim_tid
+  select * from source_dim_tid
 )
 
 select * from final
