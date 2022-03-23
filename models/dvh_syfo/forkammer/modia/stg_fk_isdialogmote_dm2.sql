addef09d7b07c76bd5bfe0cb1be2ec3eee57b171
Patrick Pedersen <patrick.pedersen@nav.no>
2022-03-23 11:03:31 +0100

    Refactor style in stg_fk_isdialogmote_dm2

--- models/dvh_syfo/forkammer/modia/stg_fk_isdialogmote_dm2.sql
+++ models/dvh_syfo/forkammer/modia/stg_fk_isdialogmote_dm2.sql
@@ -1,9 +1,9 @@
 with source_fs_dialogmote as (
-    select  * from {{ source('dmx_pox_dialogmote', 'FK_ISDIALOGMOTE_DM2') }}
+  select * from {{ source('dmx_pox_dialogmote', 'FK_ISDIALOGMOTE_DM2') }}
 ),
 
 final as (
-    select * from source_fs_dialogmote
+  select * from source_fs_dialogmote
 )
 
 select * from final
