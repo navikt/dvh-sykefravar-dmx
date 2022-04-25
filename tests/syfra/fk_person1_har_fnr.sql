9c88df07f484823c99188c17c861d37e9fb3e528
Patrick Pedersen <patrick.pedersen@nav.no>
2022-04-25 11:21:23 +0200

    Test if fk_person1 has off_id

---
+++ tests/syfra/fk_person1_har_fnr.sql
@@ -0,0 +1,13 @@
+{{ config(
+    tags="syfra"
+) }}
+
+SELECT test_table.fk_person1
+FROM {{ ref('ssb_syfra_teller_test') }} test_table
+LEFT JOIN
+  {{ source('dt_person', 'dvh_person_ident_off_id') }} person_ident ON
+    test_table.fk_person1 = person_ident.fk_person1
+WHERE test_table.kvartal = {{ var('kvartal', 202101) }}
+  AND person_ident.gyldig_fra_dato < test_table.p_start
+  AND person_ident.gyldig_til_dato > test_table.p_slutt
+  AND person_ident.off_id IS NULL
