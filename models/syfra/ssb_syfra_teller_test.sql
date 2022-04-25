6a87858ec34457aa9440a9bfcfc7c0a9187318e5
Patrick Pedersen <patrick.pedersen@nav.no>
2022-04-25 16:24:48 +0200

    Refactor ssb_syfra_teller_test to be style compliant

--- models/syfra/ssb_syfra_teller_test.sql
+++ models/syfra/ssb_syfra_teller_test.sql
@@ -1,21 +1,27 @@
-WITH versjon AS (
+WITH godkjente_versjoner AS (
   SELECT pk_dim_versjon FROM {{ source('dt_p', 'dim_versjon') }}
   WHERE
     tabell_navn = 'SSB_SYFRA_TELLER'
     AND status = 'GODKJENT'
 ),
 
+tellerfil AS (
+  SELECT *
+  FROM {{ source('dt_sensitiv', 'ssb_syfra_teller') }}
+),
+
 final AS (
   SELECT
-    fk_person1,
-    s_start,
-    s_stopp,
-    p_start,
-    p_slutt,
-    gj_uforg,
-    kvartal
-  FROM {{ source('dt_sensitiv', 'ssb_syfra_teller') }}
-  RIGHT JOIN versjon ON fk_dim_versjon = versjon.pk_dim_versjon
+    tellerfil.fk_person1,
+    tellerfil.s_start,
+    tellerfil.s_stopp,
+    tellerfil.p_start,
+    tellerfil.p_slutt,
+    tellerfil.gj_uforg,
+    tellerfil.kvartal
+  FROM godkjente_versjoner
+  LEFT JOIN tellerfil ON
+    tellerfil.fk_dim_versjon = godkjente_versjoner.pk_dim_versjon
 )
 
 SELECT * FROM final
