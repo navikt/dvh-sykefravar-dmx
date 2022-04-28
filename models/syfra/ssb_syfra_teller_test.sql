de626ae471fd5f5afd85c0b0d4ac16bd2f0640ac
Patrick Pedersen <patrick.pedersen@nav.no>
2022-04-28 16:42:42 +0200

    Add off_id to ssb_syfra_teller_test

--- models/syfra/ssb_syfra_teller_test.sql
+++ models/syfra/ssb_syfra_teller_test.sql
@@ -5,24 +5,35 @@ WITH godkjente_versjoner AS (
     AND status = 'GODKJENT'
 ),
 
-tellerfil AS (
+tellerfil_grunnlag AS (
   SELECT *
-  FROM {{ source('dt_sensitiv', 'ssb_syfra_teller') }}
+  FROM godkjente_versjoner
+  LEFT JOIN {{ source('dt_sensitiv', 'ssb_syfra_teller') }} tellerfil_grunnlag
+  ON
+    tellerfil_grunnlag.fk_dim_versjon = godkjente_versjoner.pk_dim_versjon
+),
+
+person_ident AS (
+  SELECT *
+  FROM {{ source('dt_person', 'dvh_person_ident_off_id') }}
 ),
 
 final AS (
   SELECT
-    tellerfil.pk_ssb_syfra_teller AS fk_ssb_syfra_teller,
-    tellerfil.fk_person1,
-    tellerfil.s_start,
-    tellerfil.s_stopp,
-    tellerfil.p_start,
-    tellerfil.p_slutt,
-    tellerfil.gj_uforg,
-    tellerfil.kvartal
-  FROM godkjente_versjoner
-  LEFT JOIN tellerfil ON
-    tellerfil.fk_dim_versjon = godkjente_versjoner.pk_dim_versjon
+    tellerfil_grunnlag.pk_ssb_syfra_teller AS fk_ssb_syfra_teller,
+    tellerfil_grunnlag.fk_person1,
+    person_ident.off_id,
+    tellerfil_grunnlag.s_start,
+    tellerfil_grunnlag.s_stopp,
+    tellerfil_grunnlag.p_start,
+    tellerfil_grunnlag.p_slutt,
+    tellerfil_grunnlag.gj_uforg,
+    tellerfil_grunnlag.kvartal
+  FROM tellerfil_grunnlag
+  LEFT JOIN person_ident
+    ON person_ident.fk_person1 = tellerfil_grunnlag.fk_person1
+    AND person_ident.gyldig_fra_dato <= tellerfil_grunnlag.p_start
+    AND person_ident.gyldig_til_dato >= tellerfil_grunnlag.p_slutt
 )
 
 SELECT * FROM final
