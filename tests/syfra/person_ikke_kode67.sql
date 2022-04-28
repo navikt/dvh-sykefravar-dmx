f3b79b4a0223bb617fb4840a4847242808c10c5c
Patrick Pedersen <patrick.pedersen@nav.no>
2022-04-28 16:44:29 +0200

    Rename fk_person1_ikke_kode67

--- tests/syfra/person_ikke_kode67.sql
+++ tests/syfra/person_ikke_kode67.sql
@@ -7,7 +7,7 @@ FROM {{ ref('ssb_syfra_teller_test') }} test_table
 LEFT JOIN
   {{ source('dt_person', 'dvh_person_ident_off_id') }} person_ident ON
     test_table.fk_person1 = person_ident.fk_person1
+    AND person_ident.gyldig_fra_dato <= test_table.p_start
+    AND person_ident.gyldig_til_dato >= test_table.p_slutt
 WHERE test_table.kvartal = {{ var('kvartal', 202101) }}
-  AND person_ident.gyldig_fra_dato < test_table.p_start
-  AND person_ident.gyldig_til_dato > test_table.p_slutt
   AND person_ident.skjermet_kode IN (6, 7)
