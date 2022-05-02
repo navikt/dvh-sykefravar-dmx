8eef6fd150287c593c12be7566d97f41b0207436
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-05-02 15:24:17 +0200

    fjerne småfeil

--- dmx_poc/airflow_dmx/dbt_run.py
+++ dmx_poc/airflow_dmx/dbt_run.py
@@ -9,14 +9,8 @@ from dataverk_vault.api import set_secrets_as_envs
 if __name__ == "__main__":
     os.environ["TZ"] = "Europe/Oslo"
     time.tzset()
-
     
-    vault_api.set_secrets_as_envs()
     #  oracle_secrets = json.loads(os.environ[os.environ["oracle_env"]])
-
-   
-
-
     set_secrets_as_envs()
     vault_api.set_secrets_as_envs()
 
@@ -24,7 +18,7 @@ if __name__ == "__main__":
     print ( os.environ["DBT_ORCL_SERVICE_U"])
     print (os.environ["DBT_ORCL_USER_U"])
     print (" file path", sys.path[0])
-
+    """
     try:
         subprocess.run(
             ["dbt", "run", "--profiles-dir", sys.path[0], "--project-dir", sys.path[0]], 
@@ -32,3 +26,6 @@ if __name__ == "__main__":
         )
     except subprocess.CalledProcessError as err:
         raise Exception(err.stdout.decode("utf-8"))
+
+    """
+    
