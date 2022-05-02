92d3740b460b57faa520df48046165b3535f864d
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-05-02 16:09:14 +0200

    for å kunne kjøre en model

--- dmx_poc/airflow_dmx/dbt_run.py
+++ dmx_poc/airflow_dmx/dbt_run.py
@@ -9,6 +9,7 @@ from dataverk_vault.api import set_secrets_as_envs
 if __name__ == "__main__":
     os.environ["TZ"] = "Europe/Oslo"
     time.tzset()
+    theModel_run  = str(sys.argv[1])
     
     #  oracle_secrets = json.loads(os.environ[os.environ["oracle_env"]])
     set_secrets_as_envs()
@@ -19,9 +20,20 @@ if __name__ == "__main__":
     print (os.environ["DBT_ORCL_USER_U"])
     print (" file path", sys.path[0])
     
+    """
     try:
         subprocess.run(
             ["dbt", "run", "--profiles-dir", sys.path[0], "--project-dir", sys.path[0]], 
+            check=True, capture_output=True
+        )
+    except subprocess.CalledProcessError as err:
+        raise Exception(err.stdout.decode("utf-8")) 
+    """
+    
+    try:
+        print ("running ", theModel_run)
+        subprocess.run(
+            ["dbt", "run -m", theModel_run,  "--profiles-dir", sys.path[0], "--project-dir", sys.path[0]], 
             check=True, capture_output=True
         )
     except subprocess.CalledProcessError as err:
