4f399a754deaab2fbfc1e23e5853c7a6a0794b2d
Patrick Pedersen <patrick.pedersen@nav.no>
2022-05-02 09:59:39 +0200

    Fix person ikke kode67 join clause
    A person should not be kode 6 or 7 inside the period + 45 days. In other words we should not take "lastet_dato" or s_start into acount.
    
    Ref.
    1: https://nav-it.slack.com/archives/CVDR2ST3R/p1651159599112209

--- tests/syfra/person_ikke_kode67.sql
+++ tests/syfra/person_ikke_kode67.sql
@@ -2,11 +2,11 @@
     tags="syfra"
 ) }}
 
-SELECT test_table.fnr
-FROM {{ ref('ssb_syfra_teller_test') }} test_table
+SELECT teller.fnr
+FROM {{ ref('ssb_syfra_teller_test') }} teller
 LEFT JOIN
   {{ source('dt_person', 'dvh_person_ident_off_id') }} person_ident ON
-    test_table.fnr = person_ident.off_id
-    AND person_ident.gyldig_fra_dato <= test_table.p_start
-    AND person_ident.gyldig_til_dato >= test_table.p_slutt
+    teller.fnr = person_ident.off_id
+    AND person_ident.gyldig_fra_dato <= teller.p_slutt + 45
+    AND person_ident.gyldig_til_dato >= teller.p_start
 WHERE person_ident.skjermet_kode IN (6, 7)
