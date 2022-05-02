3662cbe8dd1334920cb93ea57c813c674d444955
Patrick Pedersen <patrick.pedersen@nav.no>
2022-05-02 12:28:46 +0200

    Fix test "ikke overlappende s_start og s_slutt"
    The test was not giving any result on overlapping records. This was caused by
    the reference table beeing joined on the same id making the reference data
    equal.
    
    While testing the query I also noticed that
    "OR referanse.s_start BETWEEN teller.s_start AND teller.s_stopp" is a
    duplication of
    "teller.s_start BETWEEN referanse.s_start AND referanse.s_stopp"
    and could be removed.
    
    Ref.
    1: https://nav-it.slack.com/archives/CVDR2ST3R/p1651484158014559?thread_ts=1650902891.567719&cid=CVDR2ST3R

--- tests/syfra/ikke_overlappende_s_start_og_s_slutt.sql
+++ tests/syfra/ikke_overlappende_s_start_og_s_slutt.sql
@@ -3,15 +3,12 @@
 ) }}
 
 SELECT
-  teller.s_start,
-  teller.s_stopp
-FROM {{ ref('ssb_syfra_teller_test') }} teller
-LEFT JOIN {{ ref('ssb_syfra_teller_test') }} referanse ON
-  teller.id = referanse.id
-WHERE teller.id != referanse.id
-  AND teller.fnr = referanse.fnr
-  AND (
-    teller.s_start BETWEEN referanse.s_start AND referanse.s_stopp
-    OR teller.s_stopp BETWEEN referanse.s_start AND referanse.s_stopp
-    OR referanse.s_start BETWEEN teller.s_start AND teller.s_stopp
-  )
+  tilfelle.s_start,
+  tilfelle.s_stopp
+FROM {{ ref('ssb_syfra_teller_test') }} tilfelle
+LEFT JOIN {{ ref('ssb_syfra_teller_test') }} ref_tilfelle ON
+  tilfelle.id != ref_tilfelle.id
+  AND tilfelle.fnr = ref_tilfelle.fnr
+WHERE
+  tilfelle.s_start BETWEEN ref_tilfelle.s_start AND ref_tilfelle.s_stopp
+  OR tilfelle.s_stopp BETWEEN ref_tilfelle.s_start AND ref_tilfelle.s_stopp
