a90dc172b70c16f5d00b331d6598e9588b7aeebb
Patrick Pedersen <patrick.pedersen@nav.no>
2022-05-02 10:00:42 +0200

    Check for not null on all syfra tellerfil column

--- tests/syfra/fnr_ikke_null.sql
+++
@@ -1,7 +0,0 @@
-{{ config(
-    tags="syfra"
-) }}
-
-SELECT fnr
-FROM {{ ref('ssb_syfra_teller_test') }}
-WHERE fnr IS NULL
--- tests/syfra/gj_uforg_mellom_0.2_og_1.sql
+++ tests/syfra/gj_uforg_mellom_0.2_og_1.sql
@@ -4,4 +4,4 @@
 
 SELECT gj_uforg
 FROM {{ ref('ssb_syfra_teller_test') }}
-WHERE gj_uforg IS NULL OR gj_uforg NOT BETWEEN 0.2 AND 1
+WHERE gj_uforg NOT BETWEEN 0.2 AND 1
