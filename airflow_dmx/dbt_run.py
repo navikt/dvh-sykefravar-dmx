8d58547944639877f9d4cdb0da7b92362fbfde14
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-05-10 15:14:31 +0200

    fjerner test kode

--- airflow_dmx/dbt_run.py
+++ airflow_dmx/dbt_run.py
@@ -26,22 +26,23 @@ if __name__ == "__main__":
         a_file = open(my_path + "/profiles.yml")
         file_contents = a_file. read()
         print(file_contents)
+    def skriver_div():
+        print ("-- Skjema er ", fetch_schema, " lengde er ", len(fetch_schema))
+        print ("-- host  test er  ", os.environ['DBT_ORCL_HOST_U'], " lengde er ", len(os.environ['DBT_ORCL_HOST_U']))
+        print ("-- host  prod er  ", os.environ['DBT_ORCL_HOST_PROD'], " lengde er ", len(os.environ['DBT_ORCL_HOST_PROD']))
+        print ("--> user prod", os.environ['DBT_ORCL_USER_PROD_PROXY'] , " lengde = ",  len(os.environ['DBT_ORCL_USER_PROD_PROXY']))
+        print ("--> user test ", os.environ['DBT_ORCL_USER_U'], " lengde er ", len(os.environ['DBT_ORCL_USER_U']))
+        print (" --> service er test ", os.environ['DBT_ORCL_SERVICE_U']," lengde er ", len(os.environ['DBT_ORCL_SERVICE_U']))
+        print ("- > service er prod  ", os.environ['DBT_ORCL_SERVICE_PROD'], " lengde er ", len(os.environ['DBT_ORCL_SERVICE_PROD'] ))
+        print(" path profiles", sys.path[0])
 
 
     # setter miljø og korrekt skjema med riktig proxy
     os.environ["DBT_DEV"] =  fetch_environment
     os.environ['DBT_ORCL_SCHEMA'] = fetch_schema
     os.environ['DBT_ORCL_USER_PROD_PROXY'] = os.environ['DBT_ORCL_USER_PROD'] + '[' + fetch_schema + ']'
-    print ("-- Skjema er ", fetch_schema, " lengde er ", len(fetch_schema))
-    print ("-- host  test er  ", os.environ['DBT_ORCL_HOST_U'], " lengde er ", len(os.environ['DBT_ORCL_HOST_U']))
-    print ("-- host  prod er  ", os.environ['DBT_ORCL_HOST_PROD'], " lengde er ", len(os.environ['DBT_ORCL_HOST_PROD']))
-    print ("--> user prod", os.environ['DBT_ORCL_USER_PROD_PROXY'] , " lengde = ",  len(os.environ['DBT_ORCL_USER_PROD_PROXY']))
-    print ("--> user test ", os.environ['DBT_ORCL_USER_U'], " lengde er ", len(os.environ['DBT_ORCL_USER_U']))
-    print (" --> service er test ", os.environ['DBT_ORCL_SERVICE_U']," lengde er ", len(os.environ['DBT_ORCL_SERVICE_U']))
-    print ("- > service er prod  ", os.environ['DBT_ORCL_SERVICE_PROD'], " lengde er ", len(os.environ['DBT_ORCL_SERVICE_PROD'] ))
-    print(" path profiles", sys.path[0])
-    skriver_profile(sys.path[0])
-
+    
+    # setter prosjekt path for å kunne hente korrekte modeller
     project_path = os.path.dirname(os.getcwd())
     print (" prosjekt path er ", project_path)
     # Skal jeg kjøre hele modellen, ellers kjør en spesifikk modell
@@ -52,13 +53,13 @@ if __name__ == "__main__":
                 ["dbt", "run", "--profiles-dir", sys.path[0], "--project-dir", project_path],
                 check=True, capture_output=True
             )
-            skriver_profile(sys.path[0])
+
             print (output.stdout.decode("utf-8"))
             if fetch_log == 'logg':
                 skriver_logg(project_path)
             print (" Ferdig hele løpet - alle modeller")
         except subprocess.CalledProcessError as err:
-            raise Exception( skriver_logg(project_path), skriver_profile(sys.path[0]), err.stdout.decode("utf-8"))
+            raise Exception( skriver_logg(project_path), err.stdout.decode("utf-8"))
     else:
         try:
             print (" Starter modell  ---> ", theModel_run)
@@ -66,7 +67,7 @@ if __name__ == "__main__":
                 ["dbt", "run","--model", theModel_run, "--profiles-dir", sys.path[0], "--project-dir", project_path],
                 check=True, capture_output=True
             )
-            skriver_profile(sys.path[0])
+
             print (output.stdout.decode("utf-8"))
             if fetch_log == 'logg':
                 skriver_logg(project_path)
