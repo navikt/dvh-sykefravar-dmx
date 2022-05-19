11e71121b5dde10d8a69339629da2ce529097eff
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-05-19 12:45:31 +0200

    fjerner unødvendig macroer

--- macros/drop_view_table_syk.sql
+++
@@ -1,8 +0,0 @@
-{% macro drop_view_table_syk(to_be_droppet,type='view') %}
-
-{% if type=='view' %}
-
- drop view {{to_be_droppet}}
-
-{% endif %}
-{% endmacro %}
--- macros/test_not_null_syk.sql
+++
@@ -1,7 +0,0 @@
-{% macro test_not_null_syk(model,column_name) %}
-
-select count(*)
-from {{ model }}
-where {{ column_name }} is null
-
-{% endmacro %}
