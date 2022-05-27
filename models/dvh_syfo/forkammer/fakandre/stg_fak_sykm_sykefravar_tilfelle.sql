d28913089a10a890dbff3564063d7c592e81d086
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-05-27 10:47:15 +0200

    legger inn arbeisperiode som stg tabell (DK_P)

--- models/dvh_syfo/forkammer/fakandre/stg_fak_sykm_sykefravar_tilfelle.sql
+++ models/dvh_syfo/forkammer/fakandre/stg_fak_sykm_sykefravar_tilfelle.sql
@@ -1,5 +1,4 @@
 
-
 with source_fak_sykm_sykefravar_tilfelle as (
     select  *  from {{ source ('dmx_poc_sykefravar', 'FAK_SYKM_SYKEFRAVAR_TILFELLE')}}
 ),
---
+++ models/dvh_syfo/kjerne/fak_sykm_sykefravar_tilfelle_geografi.sql
@@ -0,0 +1,19 @@
+
+
+WITH fak_sykm_sykefravar_tilfelle_org AS (
+    SELECT * FROM {{ref('stg_fak_sykm_sykefravar_tilfelle')}}
+),
+
+dim_geografi AS (
+    SELECT * FROM {{ref('stg_dim_geografi')}}
+),
+
+final AS (
+    SELECT fak_sykm_sykefravar_tilfelle_org.*, dim_geografi.fylke_navn
+    FROM fak_sykm_sykefravar_tilfelle_org
+    LEFT JOIN dim_geografi
+    ON fak_sykm_sykefravar_tilfelle_org.fk_dim_geografi = dim_geografi.pk_dim_geografi
+
+)
+
+SELECt final.* FROM final
