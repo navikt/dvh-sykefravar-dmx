6b78174822f9e948dc73190d12aca7e4da8d9e8d
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-05-30 13:40:08 +0200

    meta-lag def

--- models/dvh_syfo/kjerne/fak_sykm_sykefravar_tilfelle_final.sql
+++ models/dvh_syfo/kjerne/fak_sykm_sykefravar_tilfelle_final.sql
@@ -1,24 +1,15 @@
 {{ config(
-    tags=["IA_PIA"]
-    mat
+    tags=["IA_PIA"],
+    materialized='table'
 ) }}
 
-WITH fak_sykm_sykefravar_tilfelle_fra_dto AS (
-    SELECT * FROM {{ref('fak_sykm_sykefravar_tilfelle_fra_dato')}}
-),
-
-dim_tid AS (
-    SELECT * FROM {{ref('stg_dim_tid')}}
+WITH fak_sykm_sykefravar_tilfelle_til_dto AS (
+    SELECT * FROM {{ref('fak_sykm_sykefravar_tilfelle_til_dato')}}
 ),
 
 final AS (
-    SELECT fak_sykm_sykefravar_tilfelle_fra_dto.*,
-    dim_tid.uke as til_dato_uke ,
-    dim_tid.aar_uke as til_dato_aar_uke
-    FROM fak_sykm_sykefravar_tilfelle_fra_dto
-    LEFT JOIN dim_tid
-    ON fak_sykm_sykefravar_tilfelle_fra_dto.sykefravar_til_dato=dim_tid.dato
 
+    SELECT * from fak_sykm_sykefravar_tilfelle_til_dto
 )
 
 SELECt final.* FROM final
