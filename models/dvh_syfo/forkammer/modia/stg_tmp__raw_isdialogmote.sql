a4913909c37a980b3cae6e81cdf34804ad62f258
Patrick Pedersen <patrick.pedersen@nav.no>
2022-06-28 12:21:28 +0200

    Convertere datatyper på stg_tmp__raw_isdialogmote

--- models/dvh_syfo/forkammer/modia/stg_tmp__raw_isdialogmote.sql
+++ models/dvh_syfo/forkammer/modia/stg_tmp__raw_isdialogmote.sql
@@ -5,17 +5,17 @@ WITH dialogmote AS (
 final AS (
   SELECT
     dialogmote.record.dialogmoteUuid,
-    dialogmote.record.dialogmoteTidspunkt,
+    TO_TIMESTAMP_TZ(dialogmote.record.dialogmoteTidspunkt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS dialogmoteTidspunkt,
     dialogmote.record.statusEndringType,
-    dialogmote.record.statusEndringTidspunkt,
+    TO_TIMESTAMP_TZ(dialogmote.record.statusEndringTidspunkt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE 'CET' AS statusEndringTidspunkt,
     dialogmote.record.personIdent,
     dialogmote.record.virksomhetsnummer,
     dialogmote.record.enhetNr,
     dialogmote.record.navIdent,
-    dialogmote.record.tilfelleStartdato,
-    dialogmote.record.arbeidstaker,
-    dialogmote.record.arbeidsgiver,
-    dialogmote.record.sykmelder
+    TO_TIMESTAMP_TZ(dialogmote.record.tilfelleStartdato, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS tilfelleStartdato,
+    DECODE(dialogmote.record.arbeidstaker, 'true', 1, 'false', 0) AS arbeidstaker,
+    DECODE(dialogmote.record.arbeidsgiver, 'true', 1, 'false', 0) AS arbeidsgiver,
+    DECODE(dialogmote.record.sykmelder, 'true', 1, 'false', 0) AS sykmelder
   FROM dialogmote dialogmote
 )
 SELECT * FROM final
