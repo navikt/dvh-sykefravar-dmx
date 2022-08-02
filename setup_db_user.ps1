d024838a49980177bddee51bb6052398f0617f82
Patrick Pedersen <patrick.pedersen@nav.no>
2022-08-02 11:37:06 +0200

    Støtte å ikke oppgi schema

--- setup_db_user.ps1
+++ setup_db_user.ps1
@@ -3,8 +3,12 @@ $schema = Read-Host -Prompt "Schema"
 $creds = Get-Credential
 
 $username = $creds.Username
-if ($schema) { $username += "[$schema]" }
+if ($schema) {
+  $username += "[$schema]"
+  $env:DBT_DB_SCHEMA = $schema
+} else {
+  $env:DBT_DB_SCHEMA = $username
+}
 $env:DBT_DB_USER = $username
 $env:DBT_DB_PASS = $creds.GetNetworkCredential().password
-$env:DBT_DB_SCHEMA = $schema
 $env:DBT_DB_TARGET = $target
