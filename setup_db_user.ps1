8a146394a8eb9e7169de13882006d099079e8989
Patrick Pedersen <patrick.pedersen@nav.no>
2022-08-02 10:46:03 +0200

    Legg til oppstartscript

--- setup_db_user.ps1
+++ setup_db_user.ps1
@@ -1,6 +1,10 @@
-$creds = Get-Credential
+$target = Read-Host -Prompt "Target db"
 $schema = Read-Host -Prompt "Schema"
+$creds = Get-Credential
 
-$env:DBT_DB_USER = $creds.Username
+$username = $creds.Username
+if ($schema) { $username += "[$schema]" }
+$env:DBT_DB_USER = $username
 $env:DBT_DB_PASS = $creds.GetNetworkCredential().password
 $env:DBT_DB_SCHEMA = $schema
+$env:DBT_DB_TARGET = $target
