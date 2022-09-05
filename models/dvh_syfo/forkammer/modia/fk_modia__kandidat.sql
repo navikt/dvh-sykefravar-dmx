03e4248fd22a7f295c92ac977484ca3761e3ca18
Patrick Pedersen <patrick.pedersen@nav.no>
2022-09-05 14:56:47 +0200

    Legge til dummy data for dm kandidater
    Co-Authored-By: torunnd <64555440+torunnd@users.noreply.github.com>
    Co-Authored-By: Stig Magne Henriksen <stig.m.henriksen@gmail.com>

---
+++ models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
@@ -0,0 +1,47 @@
+WITH source AS (
+  SELECT
+    'A' AS uuid,
+    TO_DATE('2022-09-01', 'YYYY-MM-DD') AS createdAt,
+    '0101190012345' AS personIdentNumber,
+    1 AS kandidat,
+    null AS arsak
+  FROM DUAL
+  UNION
+  SELECT
+      'B' AS uuid,
+      TO_DATE('2022-09-02', 'YYYY-MM-DD') AS createdAt,
+      '0201190012345' AS personIdentNumber,
+      1 AS kandidat,
+      null AS arsak
+  FROM DUAL
+  UNION
+  SELECT
+      'C' AS uuid,
+      TO_DATE('2022-09-03', 'YYYY-MM-DD') AS createdAt,
+      '0201190012345' AS personIdentNumber,
+      0 AS kandidat,
+      'UNNTAK' AS arsak
+  FROM DUAL
+  UNION
+  SELECT
+      'D' AS uuid,
+      TO_DATE('2022-09-02', 'YYYY-MM-DD') AS createdAt,
+      '0301190012345' AS personIdentNumber,
+      1 AS kandidat,
+      null AS arsak
+  FROM DUAL
+  UNION
+  SELECT
+      'E' AS uuid,
+      TO_DATE('2022-09-03', 'YYYY-MM-DD') AS createdAt,
+      '0301190012345' AS personIdentNumber,
+      0 AS kandidat,
+      'DIALOGMOTE_FERDIGSTILT' AS arsak
+  FROM DUAL
+)
+
+, final AS (
+  SELECT * FROM source
+)
+
+SELECT * FROM final
