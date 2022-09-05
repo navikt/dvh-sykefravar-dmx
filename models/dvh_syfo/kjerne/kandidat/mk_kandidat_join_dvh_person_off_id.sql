b0084593886caf1c525f9765e1a0423778376ce8
Patrick Pedersen <patrick.pedersen@nav.no>
2022-09-05 17:10:50 +0200

    Erstatte person ident med fk_person1 på kandidater

---
+++ models/dvh_syfo/kjerne/kandidat/mk_kandidat_join_dvh_person_off_id.sql
@@ -0,0 +1,32 @@
+WITH kandidat AS (
+  SELECT * FROM {{ ref('fk_modia__kandidat') }}
+)
+
+, dvh_person_ident_off_id AS (
+  SELECT * FROM {{ ref('felles_dt_person__dvh_person_ident_off_id') }}
+)
+
+, kandidat_join_fk_person1 AS (
+  SELECT
+    kandidat.*,
+    DECODE(dvh_person_ident_off_id.fk_person1, NULL, -1) AS fk_person1
+  FROM kandidat
+  LEFT JOIN dvh_person_ident_off_id
+  ON kandidat.personIdentNumber = dvh_person_ident_off_id.off_id
+  WHERE createdAt
+  BETWEEN dvh_person_ident_off_id.gyldig_fra_dato
+  AND dvh_person_ident_off_id.gyldig_til_dato
+  OR dvh_person_ident_off_id.fk_person1 IS NULL
+)
+
+, final AS (
+  SELECT
+    uuid,
+    1234 AS fk_person1, -- TODO
+    createdAt,
+    kandidat,
+    arsak
+  FROM kandidat_join_fk_person1
+)
+
+SELECT * FROM final
