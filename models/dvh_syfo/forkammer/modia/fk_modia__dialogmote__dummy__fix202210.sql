a256f903bd6baa804a11b6d2e05d6980b8697e0d
Patrick Pedersen <patrick.pedersen@nav.no>
2022-09-05 15:26:39 +0200

    Legge til dialogmote dummy data
    Co-Authored-By: torunnd <64555440+torunnd@users.noreply.github.com>

---
+++ models/dvh_syfo/forkammer/modia/fk_modia__dialogmote__dummy__fix202210.sql
@@ -0,0 +1,33 @@
+WITH source AS (
+  SELECT
+    'AA' as dialogmote_uuid,
+    TO_TIMESTAMP_TZ('2022-09-02 13', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS dialogmote_tidspunkt,
+    'INNKALT' as status_endring_type,
+    TO_TIMESTAMP_TZ('2022-09-02 09', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS status_endring_tidspunkt,
+    '0301190012345' as person_ident,
+    null as virksomhetsnr,
+    0806 as enhet_nr,
+    null as nav_ident,
+    TO_TIMESTAMP_TZ('2022-07-01', 'YYYY-MM-DD') AT TIME ZONE 'CET' AS tilfelle_startdato,
+    1 AS arbeidstaker_flagg,
+    1 AS arbeidsgiver_flagg,
+    1 AS sykmelder_flagg
+  FROM DUAL
+  UNION
+  SELECT
+      'AA' as dialogmote_uuid,
+      TO_TIMESTAMP_TZ('2022-09-02 13', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS dialogmote_tidspunkt,
+      'FERDIGSTILT' as status_endring_type,
+      TO_TIMESTAMP_TZ('2022-09-02 14', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS status_endring_tidspunkt,
+      '0301190012345' as person_ident,
+      null as virksomhetsnr,
+      0806 as enhet_nr,
+      null as nav_ident,
+      TO_TIMESTAMP_TZ('2022-07-01', 'YYYY-MM-DD') AT TIME ZONE 'CET' AS tilfelle_startdato,
+      1 AS arbeidstaker_flagg,
+      1 AS arbeidsgiver_flagg,
+      1 AS sykmelder_flagg
+  FROM DUAL
+)
+
+SELECT * FROM source
