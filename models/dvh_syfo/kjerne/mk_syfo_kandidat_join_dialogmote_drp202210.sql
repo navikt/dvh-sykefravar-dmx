8d36cdcc9653f6aeb22019866813e83c51f3862d
torunnd <64555440+torunnd@users.noreply.github.com>
2022-09-08 10:02:26 +0200

    laget to nye modeller

---
+++ models/dvh_syfo/kjerne/mk_syfo_kandidat_join_dialogmote_drp202210.sql
@@ -0,0 +1,17 @@
+with kandidat as (
+  select * from {{ ref('mk_kandidat_join_dvh_person_off_id') }}
+) ,
+
+dialogmote as (
+  select
+    fk_person1,
+    tilfelle_start_dato,
+    dilogmote_tidspunkt,
+    enhet_nr
+  from {{ ref('fk_modia_dialogmote') }}
+  where status_endring_type = 'FERDIGSTILT'
+) ,
+
+kandidat_join_dialogmote as (
+  select * from
+)
