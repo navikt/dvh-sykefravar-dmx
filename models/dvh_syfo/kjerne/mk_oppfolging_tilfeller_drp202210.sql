67f3c006abdd9e2143b458245655b369bab0ae5b
torunnd <64555440+torunnd@users.noreply.github.com>
2022-09-08 10:03:42 +0200

    ny commit for at siste fil skal komme med

---
+++ models/dvh_syfo/kjerne/mk_oppfolging_tilfeller_drp202210.sql
@@ -0,0 +1,22 @@
+with
+tilfeller_fra_kandidat as (
+  select
+    fk_person1,
+    createdAt as tilfelle_startdato --må byttes ut
+  from  {{ ref('mk_kandidat_join_dvh_person_off_id') }}
+)
+,
+tilfeller_fra_dialogmote as (
+  select
+    fk_person1,
+    tilfelle_startdato
+  from  {{ ref('fk_modia__dialogmote') }}
+)
+,--ev kan man hente tilfeller fra arena også?
+final as (
+  select distinct fk_person1, tilfelle_startdato from (
+  select * from tilfeller_fra_dialogmote
+  union
+  select * from tilfeller_fra_kandidat)
+)
+select * from final
