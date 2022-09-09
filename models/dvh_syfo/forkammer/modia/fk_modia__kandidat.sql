387cb618a54f2cd389ba582a068334be48d50d56
Patrick Pedersen <patrick.pedersen@nav.no>
2022-09-09 12:50:58 +0200

    Legge til tilfelle_startdato på kandidat
    Co-Authored-By: torunnd <64555440+torunnd@users.noreply.github.com>

--- models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
@@ -3,42 +3,43 @@ WITH source AS (
   SELECT
     'A' AS uuid,
     TO_DATE('2022-09-01', 'YYYY-MM-DD') AS createdAt,
+    TO_DATE('2022-02-10', 'YYYY-MM-DD') AS tilfelle_startdato,
     '0101190012345' AS personIdentNumber,
     1 AS kandidat,
-    null AS arsak
+    'STOPPUNKT' AS arsak
   FROM DUAL
-  UNION
-  SELECT
-      'B' AS uuid,
-      TO_DATE('2022-09-02', 'YYYY-MM-DD') AS createdAt,
-      '0201190012345' AS personIdentNumber,
-      1 AS kandidat,
-      null AS arsak
-  FROM DUAL
-  UNION
-  SELECT
-      'C' AS uuid,
-      TO_DATE('2022-09-03', 'YYYY-MM-DD') AS createdAt,
-      '0201190012345' AS personIdentNumber,
-      0 AS kandidat,
-      'UNNTAK' AS arsak
-  FROM DUAL
-  UNION
-  SELECT
-      'D' AS uuid,
-      TO_DATE('2022-09-02', 'YYYY-MM-DD') AS createdAt,
-      '0301190012345' AS personIdentNumber,
-      1 AS kandidat,
-      null AS arsak
-  FROM DUAL
-  UNION
-  SELECT
-      'E' AS uuid,
-      TO_DATE('2022-09-03', 'YYYY-MM-DD') AS createdAt,
-      '0301190012345' AS personIdentNumber,
-      0 AS kandidat,
-      'DIALOGMOTE_FERDIGSTILT' AS arsak
-  FROM DUAL
+--  UNION
+--  SELECT
+--      'B' AS uuid,
+--      TO_DATE('2022-09-02', 'YYYY-MM-DD') AS createdAt,
+--      '0201190012345' AS personIdentNumber,
+--      1 AS kandidat,
+--      'STOPPUNKT' AS arsak
+--  FROM DUAL
+--  UNION
+--  SELECT
+--      'C' AS uuid,
+--      TO_DATE('2022-09-03', 'YYYY-MM-DD') AS createdAt,
+--      '0201190012345' AS personIdentNumber,
+--      0 AS kandidat,
+--      'UNNTAK' AS arsak
+--  FROM DUAL
+--  UNION
+--  SELECT
+--      'D' AS uuid,
+--      TO_DATE('2022-09-02', 'YYYY-MM-DD') AS createdAt,
+--      '0301190012345' AS personIdentNumber,
+--      1 AS kandidat,
+--      'STOPPUNKT' AS arsak
+--  FROM DUAL
+--  UNION
+--  SELECT
+--      'E' AS uuid,
+--      TO_DATE('2022-09-03', 'YYYY-MM-DD') AS createdAt,
+--      '0301190012345' AS personIdentNumber,
+--      0 AS kandidat,
+--      'DIALOGMOTE_FERDIGSTILT' AS arsak
+--  FROM DUAL
 )
 
 , final AS (
