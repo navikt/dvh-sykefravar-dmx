56029227fd7e7668797cd7d2c6d51124d325ca31
Patrick Pedersen <patrick.pedersen@nav.no>
2022-09-09 13:08:42 +0200

    Endre dummy data i mk_syfo_tilfeller
    Co-Authored-By: torunnd <64555440+torunnd@users.noreply.github.com>

--- models/dvh_syfo/kjerne/mk_syfo_tilfeller_drp202210.sql
+++ models/dvh_syfo/kjerne/mk_syfo_tilfeller_drp202210.sql
@@ -1,22 +1,26 @@
 with
 tilfeller_fra_kandidat as (
   select
-    fk_person1,
-    (createdat - 49) as tilfelle_startdato -- TODO må byttes ut
+    *
   from  {{ ref('mk_kandidat_join_dvh_person_off_id') }}
 )
 ,
 tilfeller_fra_dialogmote as (
   select
-    fk_person1,
-    tilfelle_startdato
-  from  {{ ref('fk_modia__dialogmote') }}
+    *
+  from  {{ ref('fk_modia__dialogmote__dummy__fix202210') }} -- TODO
 )
 ,--ev kan man hente tilfeller fra arena også?
 final as (
   select distinct fk_person1, tilfelle_startdato from (
-  select * from tilfeller_fra_dialogmote
+  select
+    fk_person1,
+    tilfelle_startdato
+  from tilfeller_fra_dialogmote
   union
-  select * from tilfeller_fra_kandidat)
+  select
+    fk_person1,
+    tilfelle_startdato
+  from tilfeller_fra_kandidat)
 )
 select * from final
