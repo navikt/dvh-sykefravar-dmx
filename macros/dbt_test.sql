9ca9991cdd176364d4fd1ef64dbe8a35cd2680b8
Patrick Pedersen <patrick.pedersen@nav.no>
2022-09-11 16:00:00 +0200

    Prefikse mock seeds med mock_

--- macros/dbt_test.sql
+++ macros/dbt_test.sql
@@ -1,6 +1,6 @@
 {% macro dbt_test_source(source, table_name) %}
   {% if target.name == "dbt_test" %}
-  {{ ref(source+"__"+table_name) }}
+  {{ ref("mock_"+source+"__"+table_name) }}
   {% else %}
   {{ source(source, table_name) }}
   {% endif %}
--- models/dvh_syfo/forkammer/modia/fk_modia__dialogmote__dummy__fix202210.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__dialogmote__dummy__fix202210.sql
@@ -1,42 +1,6 @@
 WITH source AS (
   -- TODO
-  SELECT
-    'AA' as dialogmote_uuid,
-    TO_TIMESTAMP_TZ('2022-09-02 13', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS dialogmote_tidspunkt,
-    'INNKALT' as status_endring_type,
-    TO_TIMESTAMP_TZ('2022-09-02 09', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS status_endring_tidspunkt,
-    2 as fk_person1,
-    null as virksomhetsnr,
-    0806 as enhet_nr,
-    null as nav_ident,
-    TO_TIMESTAMP_TZ('2022-07-01', 'YYYY-MM-DD') AT TIME ZONE 'CET' AS tilfelle_startdato,
-    1 AS arbeidstaker_flagg,
-    1 AS arbeidsgiver_flagg,
-    1 AS sykmelder_flagg
-  FROM DUAL
-  UNION
-  SELECT
-      'AA' as dialogmote_uuid,
-      TO_TIMESTAMP_TZ('2022-09-02 13', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS dialogmote_tidspunkt,
-      'FERDIGSTILT' as status_endring_type,
-      TO_TIMESTAMP_TZ('2022-09-02 14', 'YYYY-MM-DD HH24') AT TIME ZONE 'CET' AS status_endring_tidspunkt,
-      2 AS fk_person1,
-      null as virksomhetsnr,
-      0806 as enhet_nr,
-      null as nav_ident,
-      TO_TIMESTAMP_TZ('2022-07-01', 'YYYY-MM-DD') AT TIME ZONE 'CET' AS tilfelle_startdato,
-      1 AS arbeidstaker_flagg,
-      1 AS arbeidsgiver_flagg,
-      1 AS sykmelder_flagg
-  FROM DUAL
+  SELECT * FROM {{ dbt_test_source("modia", "dialogmote") }}
 )
 
-, seeds AS (
-  SELECT * FROM {{ ref('modia_dialogmote') }}
-)
-
-, final AS (
-  SELECT * FROM seeds
-)
-
-SELECT * FROM final
+SELECT * FROM source
