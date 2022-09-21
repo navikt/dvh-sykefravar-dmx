f500ed3a95f2474af976c7be63986002d11af93f
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-09-21 11:19:24 +0200

    leser inn kandidater fra json

---
+++ models/dvh_syfo/forkammer/modia/base_modia__kandidater.sql
@@ -0,0 +1,50 @@
+WITH kandidater AS (
+  SELECT * FROM {{ source('modia', 'raw_isdialogmotekandidat') }}
+)
+
+, final AS (
+  SELECT
+    kandidater.kafka_message.uuid as kandidater_uuid,
+    TO_TIMESTAMP_TZ(kandidater.kafka_message.createdAt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS createdAt,
+    kandidater.kafka_message.personIdent as person_ident,
+    DECODE(kandidater.kafka_message.kandidat, 'true', 1, 'false', 0) AS kandidat_flagg,
+    kandidater.kafka_message.arsak as arsak,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    kildesystem
+  FROM kandidater kandidater
+)
+
+SELECT * FROM final
+
+/*
+      kandidater.kafka_message.kandidaterUuid as kandidater_uuid,
+   TO_TIMESTAMP_TZ(kandidater.kafka_message.createdAt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS createdAt,
+    kandidater.kafka_message.personIdent as person_ident,
+    DECODE(kandidater.kafka_message.kandidat, 'true', 1, 'false', 0) AS kandidat_flagg,
+    kandidater.kafka_message.arsak as arsak,
+{"uuid": "60a259ee-ac5d-42fa-8d92-87b77788d961", "createdAt": "2022-06-30T01:37:00.122345206Z", "personIdentNumber": "04867799392", "kandidat": true, "arsak": "STOPPUNKT"}
+create table RAW_ISkandidater
+(
+	KAFKA_HASH VARCHAR2(255 char),
+	KAFKA_MESSAGE CLOB
+		constraint RAW_ISDIALOGMOTEKANDIDAT_KAFKA_MESSAGE_JSON
+			check (kafka_message is json),
+	KAFKA_TOPIC VARCHAR2(255 char),
+	KAFKA_PARTISJON NUMBER,
+	KAFKA_OFFSET NUMBER,
+	KAFKA_KEY VARCHAR2(255 char),
+	KAFKA_TIMESTAMP NUMBER,
+	KAFKA_MOTTATT_DATO DATE,
+	OPPDATERT_DATO DATE,
+	LASTET_DATO DATE,
+	KILDESYSTEM VARCHAR2(50 char)
+)
+*/
+
+
+
+
