09cfa68ea3f9ad7a3617d635411a821e49bbccd9
torunnd <64555440+torunnd@users.noreply.github.com>
2022-09-21 13:58:40 +0200

    lagt til modell som henter syfo210 hendelser og legger til periode

---
+++ models/dvh_syfo/kjerne/mk_hendelser_syfo210.sql
@@ -0,0 +1,24 @@
+/************************************************'
+*I denne modellen velges kun hendelser relevante for syfo210
+*rapport_periode beregnes (= dm tidspunkt for avholdte dm og
+*passering av 26 uker for kandidathendelser)
+**************************************************/
+WITH hendelser as (
+  SELECT
+    *
+  FROM {{ ref('mk_syfo__union') }}
+)
+,
+hendelser_m_periode as (
+  SELECT
+    hendelser.*,
+    decode(hendelse, 'FERDIGSTILT', to_char(dialogmote_tidspunkt, 'YYYYMM'), to_char(tilfelle_startdato1 + 26*7,'YYYYMM')) as periode
+  FROM hendelser
+)
+,
+final as (
+  SELECT
+  *
+  FROM hendelser_m_periode where hendelse in ('FERDIGSTILT', 'STOPPUNKT','DIALOGMOTE_FERDIGSTILT','UNNTAK')
+)
+select * from final
