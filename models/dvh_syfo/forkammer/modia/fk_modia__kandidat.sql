3bcdcfa00cd5e99b0c69c136eba176de111af686
Patrick Pedersen <patrick.pedersen@nav.no>
2022-10-03 13:36:27 +0200

    Fjerne mock kilder
    Co-Authored-By: torunnd <64555440+torunnd@users.noreply.github.com>
    Co-Authored-By: Stig Magne Henriksen <stig.m.henriksen@gmail.com>

--- models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
@@ -1,6 +1,20 @@
-WITH source AS (
-  -- TODO
-  SELECT * FROM {{ dbt_test_source("modia", "kandidat") }}
+WITH kandidater AS (
+  SELECT * FROM {{ source('modia', 'raw_isdialogmotekandidat') }}
 )
-
-SELECT * FROM source
+, final AS (
+  SELECT
+    kandidater.kafka_message.uuid as kilde_uuid,
+    TO_TIMESTAMP(kandidater.kafka_message.createdAt, 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm') AT TIME ZONE 'CET' AS createdAt,
+    kandidater.kafka_message.personIdentNumber as person_ident,
+    DECODE(kandidater.kafka_message.kandidat, 'true', 1, 'false', 0) AS kandidat_flagg,
+    kandidater.kafka_message.arsak as arsak,
+    TO_DATE(kandidater.kafka_message.tilfelleStart, 'YYYY-MM-DD') as tilfelle_startdato,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    kildesystem
+  FROM kandidater kandidater
+)
+SELECT * FROM final
--- models/dvh_syfo/kjerne/mk_syfo__union.sql
+++ models/dvh_syfo/kjerne/mk_syfo__union.sql
@@ -7,7 +7,7 @@
 *********************************************************/
 WITH union_all AS (
   {{ dbt_utils.union_relations(
-    relations=[ref('fk_modia__kandidat'), ref('fk_modia__dialogmote__dummy__fix202210')],
+    relations=[ref('fk_modia__kandidat'), ref('fk_modia__dialogmote')],
     source_column_name=None
   ) }}
 )
@@ -15,9 +15,6 @@ WITH union_all AS (
 union_all_decode_hendelse as (
   SELECT
     union_all.*,
-    decode(uuid, null, dialogmote_uuid, uuid) as kilde_uuid,
-    decode(personIdentNumber, null,person_ident_number, personIdentNumber) as fk_person1,
-    decode(tilfelle_startdato, null,tilfelleStartdato, tilfelle_startdato) as tilfelle_startdato1,
     decode(arsak, null, status_endring_type, arsak) as hendelse,
     decode(createdAt,null,status_endring_tidspunkt, createdAt) as hendelse_tidspunkt
   FROM union_all
@@ -25,17 +22,16 @@ union_all_decode_hendelse as (
 ,
 final as (
     SELECT
-      fk_person1,
-      tilfelle_startdato1,
+      person_ident,
+      tilfelle_startdato,
       hendelse,
       hendelse_tidspunkt,
       dialogmote_tidspunkt,
       enhet_nr,
-      arbeidstaker,
-      arbeidsgiver,
-      sykmelder,
+      arbeidstaker_flagg,
+      arbeidsgiver_flagg,
+      sykmelder_flagg,
       kilde_uuid
     FROM union_all_decode_hendelse
 )
 select * from final
-
