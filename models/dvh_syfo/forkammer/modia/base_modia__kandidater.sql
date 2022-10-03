c0648cd895bba3caa81c4f6c2991a88fd309669f
torunnd <64555440+torunnd@users.noreply.github.com>
2022-10-03 12:21:12 +0200

    legger til modeller for å pakke ut json og omkoding til fk_person1

---
+++ models/dvh_syfo/forkammer/modia/base_modia__kandidater.sql
@@ -0,0 +1,19 @@
+WITH kandidater AS (
+  SELECT * FROM {{ source('modia', 'raw_isdialogmotekandidat') }}
+)
+, final AS (
+  SELECT
+    kandidater.kafka_message.uuid as kandidater_uuid,
+    TO_TIMESTAMP(kandidater.kafka_message.createdAt, 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm') AT TIME ZONE 'CET' AS createdAt,
+    kandidater.kafka_message.personIdentNumber as person_ident,
+    DECODE(kandidater.kafka_message.kandidat, 'true', 1, 'false', 0) AS kandidat_flagg,
+    kandidater.kafka_message.arsak as arsak,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    kildesystem
+  FROM kandidater kandidater
+)
+SELECT * FROM final
---
+++ models/dvh_syfo/forkammer/modia/base_modia__kandidater_fkperson1.sql
@@ -0,0 +1,29 @@
+{{config(
+    materialized='table'
+)}}
+WITH kandidater_base AS (
+  SELECT * FROM {{ ref('base_modia__kandidater') }}
+),
+dim_off_id AS (
+    SELECT * FROM {{ref('felles_dt_person__dvh_person_ident_off_id') }}
+),
+final as
+(
+    SELECT
+    kandidater_uuid,
+    createdAt,
+    kandidat_flagg,
+    arsak,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    kildesystem,
+    dim_off_id.fk_person1 as fk_person1
+    FROM kandidater_base
+    LEFT JOIN dim_off_id
+    ON kandidater_base.person_ident = dim_off_id.off_id
+    where kandidater_base.kafka_mottatt_dato BETWEEN dim_off_id.gyldig_fra_dato AND dim_off_id.gyldig_til_dato
+)
+SELECT * FROM final
