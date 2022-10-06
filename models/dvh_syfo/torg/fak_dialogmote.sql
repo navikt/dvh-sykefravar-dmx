04c7f12045c7eeb08a4b102bec864c590c12991a
Elisabeth Nes <49159822+elisnes@users.noreply.github.com>
2022-10-06 15:34:06 +0200

    Opprettet fak_dialogmote
    Co-Authored-By: torunnd <64555440+torunnd@users.noreply.github.com>
    Co-Authored-By: Patrick Pedersen <patrick@pedersen.biz>

--- models/dvh_syfo/torg/fak_dialogmote.sql
+++ models/dvh_syfo/torg/fak_dialogmote.sql
@@ -1,16 +1,23 @@
 WITH hendelser AS (
+  SELECT * FROM {{ref("mk_syfo210_hendelser_pvt")}}
+)
+
+
+
+
+,final AS (
   SELECT
-    1234 AS fk_person1
-    ,TO_DATE('9999-12-31', 'YYYY-MM-DD') AS tilfelle_startdato
+    fk_person1
+    ,tilfelle_startdato
     ,1234 AS nav_enhet
     ,1 AS dialogmote2_innen_26_uker_flagg
     ,0 AS svar_behov
     ,'Behandler/arbeidsgiver/Sykmeldt/tom' AS svar_behov_hvem
     ,TO_DATE('9999-12-31', 'YYYY-MM-DD') AS behov_meldt_dato
     ,'Behandler/arbeidsgiver/Sykmeldt/tom' AS behov_meldt_hvem
-    ,TO_DATE('9999-12-31', 'YYYY-MM-DD') AS dialogmote_avholdt_dato
+    ,dialogmote_tidspunkt1 AS dialogmote2_avholdt_dato
     ,2 AS dialogmote_nr
     ,0 AS unntak
-  FROM dual
+  FROM hendelser
 )
-SELECT * FROM hendelser
+SELECT * FROM final
