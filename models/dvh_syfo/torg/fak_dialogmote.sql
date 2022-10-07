940455b93fcdcfbda2f710f58a1cb9206f8b31e9
Patrick Pedersen <patrick.pedersen@nav.no>
2022-10-07 10:20:47 +0200

    Reformater fak_dialogmote

--- models/dvh_syfo/torg/fak_dialogmote.sql
+++ models/dvh_syfo/torg/fak_dialogmote.sql
@@ -1,17 +1,19 @@
 WITH hendelser AS (
   SELECT * FROM {{ref("mk_syfo210_hendelser_pvt")}}
 )
+
 ,dim_person1 AS (
   SELECT * FROM {{ ref('felles_dt_person__dim_person1') }}
 )
-,flag_innen_26Uker as (
-  select fk_person1, tilfelle_startdato,
-  CASE
+
+,flag_innen_26Uker AS (
+  SELECT fk_person1, tilfelle_startdato,
+    CASE
       WHEN dialogmote_tidspunkt1 IS NULL THEN NULL
       WHEN dialogmote_tidspunkt1 < (tilfelle_startdato + 26*7) THEN 1
       ELSE 0
     END AS dialogmote2_innen_26_uker_flagg
-   from hendelser
+  FROM hendelser
 )
 
 ,final AS (
@@ -19,7 +21,7 @@ WITH hendelser AS (
     hendelser.fk_person1
     ,hendelser.tilfelle_startdato
     ,dialogmote2_innen_26_uker_flagg
-    ,0 AS svar_behov
+    ,NULL AS svar_behov
     ,NULL AS svar_behov_hvem
     ,NULL AS behov_meldt_dato
     ,NULL AS behov_meldt_hvem
@@ -39,14 +41,13 @@ WITH hendelser AS (
     ,TO_NUMBER(
       TO_CHAR(unntak, 'YYYYMMDD')
     ) AS fk_dim_tid__unntak_dato
-
   FROM hendelser
   LEFT JOIN dim_person1 ON
-    hendelser.fk_person1 = dim_person1.fk_person1
-    AND hendelser.tilfelle_startdato BETWEEN dim_person1.gyldig_fra_dato AND dim_person1.gyldig_til_dato
-  left join flag_innen_26Uker on
-    hendelser.fk_person1 = flag_innen_26Uker.fk_person1
-    and hendelser.tilfelle_startdato = flag_innen_26Uker.tilfelle_startdato
+    hendelser.fk_person1 = dim_person1.fk_person1 AND
+    hendelser.tilfelle_startdato BETWEEN dim_person1.gyldig_fra_dato AND dim_person1.gyldig_til_dato
+  LEFT JOIN flag_innen_26Uker ON
+    hendelser.fk_person1 = flag_innen_26Uker.fk_person1 AND
+    hendelser.tilfelle_startdato = flag_innen_26Uker.tilfelle_startdato
 )
 
 SELECT * FROM final
