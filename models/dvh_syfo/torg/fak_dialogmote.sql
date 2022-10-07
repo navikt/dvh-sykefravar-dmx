378c6f3a7ac677bb26f26b8e8af6027fd18face0
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2022-10-07 10:16:32 +0200

    legge inn flagg 26 uker

--- models/dvh_syfo/torg/fak_dialogmote.sql
+++ models/dvh_syfo/torg/fak_dialogmote.sql
@@ -4,11 +4,21 @@ WITH hendelser AS (
 ,dim_person1 AS (
   SELECT * FROM {{ ref('felles_dt_person__dim_person1') }}
 )
+,flag_innen_26Uker as (
+  select fk_person1, tilfelle_startdato,
+  CASE
+      WHEN dialogmote_tidspunkt1 IS NULL THEN NULL
+      WHEN dialogmote_tidspunkt1 < (tilfelle_startdato + 26*7) THEN 1
+      ELSE 0
+    END AS dialogmote2_innen_26_uker_flagg
+   from hendelser
+)
+
 ,final AS (
   SELECT
     hendelser.fk_person1
-    ,tilfelle_startdato
-    ,1 AS dialogmote2_innen_26_uker_flagg
+    ,hendelser.tilfelle_startdato
+    ,dialogmote2_innen_26_uker_flagg
     ,0 AS svar_behov
     ,NULL AS svar_behov_hvem
     ,NULL AS behov_meldt_dato
@@ -18,7 +28,7 @@ WITH hendelser AS (
     ,unntak AS unntak_dato
     ,dim_person1.fk_dim_organisasjon
     ,TO_NUMBER(
-      TO_CHAR(tilfelle_startdato, 'YYYYMMDD')
+      TO_CHAR(hendelser.tilfelle_startdato, 'YYYYMMDD')
     ) AS fk_dim_tid__tilfelle_startdato
     ,TO_NUMBER(
       TO_CHAR(dialogmote_tidspunkt1, 'YYYYMMDD')
@@ -29,9 +39,14 @@ WITH hendelser AS (
     ,TO_NUMBER(
       TO_CHAR(unntak, 'YYYYMMDD')
     ) AS fk_dim_tid__unntak_dato
+
   FROM hendelser
   LEFT JOIN dim_person1 ON
     hendelser.fk_person1 = dim_person1.fk_person1
     AND hendelser.tilfelle_startdato BETWEEN dim_person1.gyldig_fra_dato AND dim_person1.gyldig_til_dato
+  left join flag_innen_26Uker on
+    hendelser.fk_person1 = flag_innen_26Uker.fk_person1
+    and hendelser.tilfelle_startdato = flag_innen_26Uker.tilfelle_startdato
 )
+
 SELECT * FROM final
