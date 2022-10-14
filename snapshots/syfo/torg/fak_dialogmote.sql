e6153ebea4a7c1dba3c3c6d3ba4c319017e9350c
Patrick Pedersen <patrick.pedersen@nav.no>
2022-10-14 15:30:01 +0200

    Historisere fak_dialogmote

---
+++ snapshots/syfo/torg/fak_dialogmote.sql
@@ -0,0 +1,19 @@
+{% snapshot fak_dialogmote__snapshot%}
+
+{{
+    config(
+      target_schema='dvh_syfo',
+      unique_key='id',
+      strategy='check',
+      check_cols='all',
+      invalidate_hard_deletes=True
+    )
+}}
+
+SELECT
+  TO_CHAR(fk_person1) || '-' || TO_CHAR(tilfelle_startdato) AS id
+  ,dialogmote.*
+FROM
+  {{ ref("fak_dialogmote") }} dialogmote
+
+{% endsnapshot %}
