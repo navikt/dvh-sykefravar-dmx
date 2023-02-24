2aa1590cd8b56d7d08d2e44cc2287182e0c30017
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-02-24 12:53:02 +0100

    Revert "fjerner union ikke nødvendig"
    This reverts commit 4eb991eed6263225acfefc1a0ee8ee8590ab8946.

--- models/dvh_syfo/kjerne/mk_modia__motebehov_enhet.sql
+++ models/dvh_syfo/kjerne/mk_modia__motebehov_enhet.sql
@@ -13,7 +13,53 @@ WITH motebehov_2 AS (
       motebehov_2.sm_fnr = motebehov_innles.fnr
       where motebehov_innles.tildelt_enhet is not null
 )
-
+,del1_med_nye_enheter AS (
+ select ny_motebehov.*
+  from ny_motebehov
+  where ny_motebehov.tildelt_enhet is not null
+)
+,del2_tar_med_null AS (
+  SELECT ny_motebehov.*,
+        motebehov_2.tildelt_enhet as tildelt_enhet_null
+        from ny_motebehov
+    LEFT JOIN motebehov_2
+    ON
+      ny_motebehov.sm_fnr= motebehov_2.sm_fnr
+    where ny_motebehov.tildelt_enhet is null
+)
+,total_med_null_og_opprinnelig  AS (
+  select MOTEBEHOV_UUID,
+        OPPRETTET_DATO,
+        OPPRETTET_AV,
+        AKTOER_ID,
+        VIRKSOMHETSNUMMER,
+        HAR_MOTEBEHOV,
+        BEHANDLET_TIDSPUNKT,
+        BEHANDLET_VEILEDER_IDENT,
+        SKJEMATYPE,
+        SM_FNR,
+        OPPRETTET_AV_FNR,
+        LASTET_DATO,
+        KILDESYSTEM,
+        tildelt_enhet_ny as endelig_enhet
+  from del1_med_nye_enheter
+    union
+  select MOTEBEHOV_UUID,
+        OPPRETTET_DATO,
+        OPPRETTET_AV,
+        AKTOER_ID,
+        VIRKSOMHETSNUMMER,
+        HAR_MOTEBEHOV,
+        BEHANDLET_TIDSPUNKT,
+        BEHANDLET_VEILEDER_IDENT,
+        SKJEMATYPE,
+        SM_FNR,
+        OPPRETTET_AV_FNR,
+        LASTET_DATO,
+        KILDESYSTEM,
+        tildelt_enhet_null as endelig_enhet
+    from del2_tar_med_null
+)
  ,final AS (
    select
       MOTEBEHOV_UUID,
@@ -29,7 +75,7 @@ WITH motebehov_2 AS (
         OPPRETTET_AV_FNR,
         LASTET_DATO,
         KILDESYSTEM,
-        tildelt_enhet_ny as tildelt_enhet
+        endelig_enhet as tildelt_enhet
         from total_med_null_og_opprinnelig
  )
 
