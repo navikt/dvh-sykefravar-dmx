d8475cc6d2896a81dbcaac8c3029cbc643c43440
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-03-07 08:24:43 +0100

    endringer neht - gyldig for kandidater og møtebehov

--- models/dvh_syfo/kjerne/mk_modia__motebehov_enhet.sql
+++ models/dvh_syfo/kjerne/mk_modia__motebehov_enhet.sql
@@ -1,82 +1,36 @@
-WITH motebehov_2 AS (
-  SELECT * FROM {{ ref('fk_modia__motebehov') }}
+WITH kandidat_2 AS (
+  SELECT * FROM {{ ref('fk_modia__kandidat') }}
+  where tildelt_enhet is not null
 )
 ,motebehov_innles AS (
-    SELECT * FROM {{ref('fk_person_oversikt_status_base') }}
+    SELECT * FROM {{ref('fk_person_oversikt_status_base')}}
 )
 ,ny_motebehov  AS (
-    SELECT motebehov_2.*,
+    SELECT kandidat_2.*,
         motebehov_innles.tildelt_enhet as tildelt_enhet_ny
-        from motebehov_2
+        from kandidat_2
     LEFT JOIN motebehov_innles
     ON
-      motebehov_2.sm_fnr = motebehov_innles.fnr
-      where motebehov_innles.tildelt_enhet is not null
-)
-,del1_med_nye_enheter AS (
- select ny_motebehov.*
-  from ny_motebehov
-  where ny_motebehov.tildelt_enhet is not null
-)
-,del2_tar_med_null AS (
-  SELECT ny_motebehov.*,
-        motebehov_2.tildelt_enhet as tildelt_enhet_null
-        from ny_motebehov
-    LEFT JOIN motebehov_2
-    ON
-      ny_motebehov.sm_fnr= motebehov_2.sm_fnr
-    where ny_motebehov.tildelt_enhet is null
-)
-,total_med_null_og_opprinnelig  AS (
-  select MOTEBEHOV_UUID,
-        OPPRETTET_DATO,
-        OPPRETTET_AV,
-        AKTOER_ID,
-        VIRKSOMHETSNUMMER,
-        HAR_MOTEBEHOV,
-        BEHANDLET_TIDSPUNKT,
-        BEHANDLET_VEILEDER_IDENT,
-        SKJEMATYPE,
-        SM_FNR,
-        OPPRETTET_AV_FNR,
-        LASTET_DATO,
-        KILDESYSTEM,
-        tildelt_enhet_ny as endelig_enhet
-  from del1_med_nye_enheter
-    union
-  select MOTEBEHOV_UUID,
-        OPPRETTET_DATO,
-        OPPRETTET_AV,
-        AKTOER_ID,
-        VIRKSOMHETSNUMMER,
-        HAR_MOTEBEHOV,
-        BEHANDLET_TIDSPUNKT,
-        BEHANDLET_VEILEDER_IDENT,
-        SKJEMATYPE,
-        SM_FNR,
-        OPPRETTET_AV_FNR,
-        LASTET_DATO,
-        KILDESYSTEM,
-        tildelt_enhet_null as endelig_enhet
-    from del2_tar_med_null
+      kandidat_2.sm_fnr = motebehov_innles.fnr
+
 )
  ,final AS (
    select
-      MOTEBEHOV_UUID,
-      OPPRETTET_DATO,
-        OPPRETTET_AV,
-        AKTOER_ID,
-        VIRKSOMHETSNUMMER,
-        HAR_MOTEBEHOV,
-        BEHANDLET_TIDSPUNKT,
-        BEHANDLET_VEILEDER_IDENT,
-        SKJEMATYPE,
-        SM_FNR,
-        OPPRETTET_AV_FNR,
-        LASTET_DATO,
-        KILDESYSTEM,
-        endelig_enhet as tildelt_enhet
-        from total_med_null_og_opprinnelig
+    kilde_uuid,
+    hendelse_tidspunkt,
+    person_ident,
+    kandidat_flagg,
+    hendelse,
+    unntakarsak,
+    tilfelle_startdato,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    kildesystem
+    case when tildelt_enhet_ny = person_ident then tildelt_enhet else tildelt_enhet_ny end as tildelt_enhet
+    from ny_motebehov
  )
 
 SELECT * FROM final
