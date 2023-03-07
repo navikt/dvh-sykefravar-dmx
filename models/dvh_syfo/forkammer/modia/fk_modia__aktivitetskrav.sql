3bde0b368c9f659ee6695aa14f40ff38829a65f8
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-03-07 12:31:06 +0100

    laget nytt view for modia: FK_MODIA__AKTIVITETSKRAV

--- models/dvh_syfo/forkammer/modia/fk_modia__aktivitetskrav.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__aktivitetskrav.sql
@@ -1,27 +1,26 @@
+
 WITH aktivitetskrav AS (
   SELECT * FROM {{ source('modia', 'raw_aktivitetskrav') }}
 )
 
 , final AS (
   SELECT
-    aktivitetskrav.kafka_message.Uuid as kilde_uuid,
-    TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.aktivitetskravTidspunkt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS aktivitetskrav_tidspunkt,
-   TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.sistVurdert, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE 'CET' AS sistVurdert,
-    TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.stoppunktAt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE 'CET' AS stoppunktAt,
+    aktivitetskrav.kafka_message.uuid as kilde_uuid,
+    TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.createdAt, 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm"Z"') AT TIME ZONE 'CET' as createdAt,
     aktivitetskrav.kafka_message.status as status,
-     aktivitetskrav.kafka_message.arsak as arsak,
     aktivitetskrav.kafka_message.beskrivelse as beskrivelse,
-    TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.createdAt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS createdAt,
+    aktivitetskrav.kafka_message.arsaker as arsaker,
+    aktivitetskrav.kafka_message.updatedBy as updatedBy,
+    TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.stoppunktAt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE 'CET' AS stoppunktAt,
+    TO_TIMESTAMP_TZ(aktivitetskrav.kafka_message.sistVurdert, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE 'CET' AS sistVurdert,
     kafka_topic,
     kafka_partisjon,
     kafka_offset,
-    kafka_key,
-    kafka_timestamp,
     kafka_mottatt_dato,
     oppdatert_dato,
     lastet_dato,
     kildesystem
-  FROM aktivitetskrav
+  FROM aktivitetskrav aktivitetskrav
 )
 
 SELECT * FROM final
