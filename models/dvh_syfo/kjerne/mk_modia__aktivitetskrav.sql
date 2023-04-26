8f398c7fcc5ca9c199faf2ae1d999353f0361377
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-04-26 15:23:36 +0200

    nå med lastet_dato

--- models/dvh_syfo/kjerne/mk_modia__aktivitetskrav.sql
+++ models/dvh_syfo/kjerne/mk_modia__aktivitetskrav.sql
@@ -1,7 +1,31 @@
+--{% set running_mnd = '2017-01-01' %}
 
-WITH aktivitetskrav_heihei as (
+WITH aktivitetskrav as (
   SELECT * FROM {{ ref("fk_modia__aktivitetskrav") }}
   where status in ('OPPFYLT','IKKE_OPPFYLT','UNNTAK')
   and LASTET_DATO < TO_DATE({{running_mnd}},'YYYY-MM-DD')
+),
+final as (
+  SELECT
+    ARSAKER,
+    CREATEDAT,
+    FK_PERSON1,
+    KAFKA_MOTTATT_DATO,
+    KAFKA_OFFSET,
+    KAFKA_PARTISJON,
+    KAFKA_TOPIC,
+    KILDE_UUID,
+    KILDESYSTEM,
+   -- LASTET_DATO,
+    TO_DATE(sysdate,'YYYY-MM-DD') as LASTET_DATO,
+    OPPDATERT_DATO,
+    SISTVURDERT,
+    STATUS,
+    STOPPUNKTAT,
+    UPDATEDBY
+from
+    aktivitetskrav
+
 )
-SELECT * FROM aktivitetskrav_heihei
+
+SELECT * FROM final
