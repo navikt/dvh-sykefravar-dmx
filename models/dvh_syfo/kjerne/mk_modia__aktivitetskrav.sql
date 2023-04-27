a46e800c8469cea13c204c6218a1c54761233b36
torunnd <64555440+torunnd@users.noreply.github.com>
2023-04-27 12:59:41 +0200

    endrer slik at man ikke faar kompileringsfeil

--- models/dvh_syfo/kjerne/mk_modia__aktivitetskrav.sql
+++ models/dvh_syfo/kjerne/mk_modia__aktivitetskrav.sql
@@ -1,7 +1,7 @@
 WITH aktivitetskrav as (
   SELECT * FROM {{ ref("fk_modia__aktivitetskrav") }}
-  where status in ("OPPFYLT","IKKE_OPPFYLT","UNNTAK")
-),
-
+  --where status in ("OPPFYLT","IKKE_OPPFYLT","UNNTAK")
+)
+select * from aktivitetskrav
 
 
