5cb6067c364134d27dd8a5de3b88a96f01442498
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-05-31 14:11:44 +0200

    leser nå med patch data

--- models/dvh_syfo/forkammer/modia/fk_modia__aktivitetskrav_raw.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__aktivitetskrav_raw.sql
@@ -1,5 +1,5 @@
 
 WITH aktivitetskrav_raw AS (
-  SELECT * FROM {{ source('modia', 'raw_aktivitetskrav_dag') }}
+  SELECT * FROM {{ source('modia', 'raw_aktivitetskrav_patch') }}
 )
 select * from aktivitetskrav_raw
