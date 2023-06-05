f842af7120aef4912215e43615c3f342e03eeb57
juliealiceskoien <julie.alice.skoien@nav.no>
2023-06-05 10:48:21 +0200

    håndterer for flere statuser

--- models/dvh_syfo/kjerne/mk_modia__aktivitetskrav.sql
+++ models/dvh_syfo/kjerne/mk_modia__aktivitetskrav.sql
@@ -24,7 +24,7 @@ WITH aktivitetskrav as (
     STATUS,
     STOPPUNKTAT,
     UPDATEDBY,
-    TO_CHAR(SISTVURDERT, 'YYYYMM') as PERIODE
+    CASE WHEN STATUS IN ('NY', 'AVVENT') then TO_CHAR(CREATEDAT, 'YYYYMM') else TO_CHAR(SISTVURDERT, 'YYYYMM') END as PERIODE
   FROM {{ ref("fk_modia__aktivitetskrav") }}
   where (
    SISTVURDERT < TO_DATE('{{var("running_mnd")}}','YYYY-MM-DD')
@@ -36,29 +36,30 @@ WITH aktivitetskrav as (
     )
 ),
 
-
 sykefravar_tilfeller as(
   select
     FK_PERSON1,
-    sykefravar_fra_dato as siste_sykefravar_startdato
+    sykefravar_fra_dato
   from {{ ref("stg_fak_sykm_sykefravar_tilfelle") }}
 ),
 
+/* WHERE clause sier at startdato for sykefraværstilfelle er tidligere enn dato for satt aktivitetskrav. */
 sorterte_sykefravarstilfeller as (
   SELECT
     aktivitetskrav.*,
-    sykefravar_tilfeller.siste_sykefravar_startdato
+    sykefravar_tilfeller.sykefravar_fra_dato
   FROM aktivitetskrav
   LEFT JOIN sykefravar_tilfeller ON sykefravar_tilfeller.FK_PERSON1 = aktivitetskrav.FK_PERSON1
-  where sykefravar_tilfeller.siste_sykefravar_startdato < aktivitetskrav.CREATEDAT
-  order by aktivitetskrav.FK_PERSON1,SISTE_SYKEFRAVAR_STARTDATO,KAFKA_MOTTATT_DATO desc
+  where sykefravar_tilfeller.sykefravar_fra_dato < aktivitetskrav.CREATEDAT
+  order by aktivitetskrav.FK_PERSON1,sykefravar_fra_dato,KAFKA_MOTTATT_DATO desc
 
 ),
 
+/* Henter siste (latest) status fra sykefraværstilfelle med row-funksjon. */
 siste_sykefravars_tilfeller as (
   SELECT
-    sorterte_sykefravarstilfeller.*,
-    ROW_NUMBER() OVER (PARTITION BY FK_PERSON1, SISTE_SYKEFRAVAR_STARTDATO, PERIODE ORDER BY KAFKA_MOTTATT_DATO desc) AS first_rownum
+    sorterte_sykefravarstilfeller.*, sorterte_sykefravarstilfeller.sykefravar_fra_dato as siste_sykefravar_startdato,
+    ROW_NUMBER() OVER (PARTITION BY FK_PERSON1, sykefravar_fra_dato, PERIODE ORDER BY KAFKA_MOTTATT_DATO desc) AS first_rownum
   FROM sorterte_sykefravarstilfeller
 
 )
