e51ac833402aa1e6d228e4a6167c50b91e4571ba
juliealiceskoien <julie.alice.skoien@nav.no>
2023-06-06 12:04:55 +0200

    Endrer source-navn

--- models/dvh_syfo/forkammer/modia/fk_modia__aktivitetskrav_raw.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__aktivitetskrav_raw.sql
@@ -1,6 +1,6 @@
 
 WITH aktivitetskrav_raw AS (
 
-    SELECT * FROM {{ source('modia', 'raw_aktivitetskrav_dag') }} -- byttest til _patch når overnevnte er i prod
+    SELECT * FROM {{ source('modia', 'raw_aktivitetskrav') }} -- byttest til _patch når overnevnte er i prod
 )
 select * from aktivitetskrav_raw
