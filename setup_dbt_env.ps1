7f4dc08fefe499df9252b2e7666ac0c8e8817a24
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-11-06 20:25:12 +0100

    ny branch for upgrade

---
+++ setup_dbt_env.ps1
@@ -0,0 +1,36 @@
+
+./.dbtenv/Scripts/activate.ps1
+
+if ($env:DBT_DB_TARGET) {
+  echo "Current setup: $env:DBT_DB_SCHEMA ($env:DBT_DB_TARGET)"
+  $changeProfile = Read-Host -Prompt "Rerun setup? (y/n)"
+  $changeProfile = $changeProfile.substring(0,1).ToLower()
+  if ($changeProfile -ne "y") {
+    exit
+  }
+}
+
+
+$target = Read-Host -Prompt "Target db"
+$target = $target.ToUpper()
+$schema = "dvh_arb_person"
+$creds = Get-Credential
+
+$username = $creds.Username
+if ($schema) {
+  $username += "[$schema]"
+  $env:DBT_DB_SCHEMA = $schema
+} else {
+  $env:DBT_DB_SCHEMA = $username
+}
+$env:DBT_ENV_SECRET_USER = $username
+$env:DBT_ENV_SECRET_PASS = $creds.GetNetworkCredential().password
+$env:DBT_DB_TARGET = $target
+
+$env:DBT_PROFILES_DIR = Get-Location
+
+
+# midlertidig fiks for thin client fra utviklerimage
+Remove-Item -Path Env:https_proxy
+$env:ORA_PYTHON_DRIVER_TYPE = "thin"
+echo "ORA_PYTHON_DRIVER_TYPE: $env:ORA_PYTHON_DRIVER_TYPE"
