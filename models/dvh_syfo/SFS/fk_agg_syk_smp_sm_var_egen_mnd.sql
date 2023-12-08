ac3e41dd6bd4cd87c7754ff2b6ac3990231b2023
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2023-12-08 08:58:44 +0100

    viewet klar for testing

--- models/dvh_syfo/SFS/fk_agg_syk_smp_sm_var_egen_mnd.sql
+++ models/dvh_syfo/SFS/fk_agg_syk_smp_sm_var_egen_mnd.sql
@@ -1,5 +1,5 @@
-WITH sourc1  (
+WITH source_egen_mnd  as (
   SELECT * from {{ source('dmx_pox_oppfolging','AGG_SYK_SMP_SM_VAR_EGEN_MND') }}
 )
 
-select * from source1
+select * from source_egen_mnd
