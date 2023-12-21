e51199160373c88968df27958eb6b849a8e6352f
juliealiceskoien <julie.alice.skoien@nav.no>
2023-12-21 10:39:18 +0100

    endrer fra navn til time zone offset pga thin mode changes mars 2023

--- models/dvh_syfo/forkammer/modia/fk_modia__dialogmote.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__dialogmote.sql
@@ -5,14 +5,14 @@ WITH dialogmote AS (
 , final AS (
   SELECT
     dialogmote.kafka_message.dialogmoteUuid as kilde_uuid,
-    TO_TIMESTAMP_TZ(dialogmote.kafka_message.dialogmoteTidspunkt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS dialogmote_tidspunkt,
+    TO_TIMESTAMP_TZ(dialogmote.kafka_message.dialogmoteTidspunkt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE '+01:00' AS dialogmote_tidspunkt,
     dialogmote.kafka_message.statusEndringType as hendelse,
-    TO_TIMESTAMP_TZ(dialogmote.kafka_message.statusEndringTidspunkt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE 'CET' AS hendelse_tidspunkt,
+    TO_TIMESTAMP_TZ(dialogmote.kafka_message.statusEndringTidspunkt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AT TIME ZONE '+01:00' AS hendelse_tidspunkt,
     dialogmote.kafka_message.personIdent as person_ident,
     dialogmote.kafka_message.virksomhetsnummer as virksomhetsnr,
     dialogmote.kafka_message.enhetNr as enhet_nr,
     dialogmote.kafka_message.navIdent as nav_ident,
-    TO_TIMESTAMP_TZ(dialogmote.kafka_message.tilfelleStartdato, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE 'CET' AS tilfelle_startdato,
+    TO_TIMESTAMP_TZ(dialogmote.kafka_message.tilfelleStartdato, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') AT TIME ZONE '+01:00' AS tilfelle_startdato,
     DECODE(dialogmote.kafka_message.arbeidstaker, 'true', 1, 'false', 0) AS arbeidstaker_flagg,
     DECODE(dialogmote.kafka_message.arbeidsgiver, 'true', 1, 'false', 0) AS arbeidsgiver_flagg,
     DECODE(dialogmote.kafka_message.sykmelder, 'true', 1, 'false', 0) AS sykmelder_flagg,
