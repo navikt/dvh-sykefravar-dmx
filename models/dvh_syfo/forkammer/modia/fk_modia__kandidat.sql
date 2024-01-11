0999c89ddf4b5eba05362f9bcb02863b0517dd01
juliealiceskoien <julie.alice.skoien@nav.no>
2024-01-11 14:49:49 +0100

    fix: cast til timestamp rettet ora-feil om timezone ved dbt execute

--- models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
@@ -4,7 +4,7 @@ WITH kandidater AS (
 , final AS (
   SELECT
     kandidater.kafka_message.uuid as kilde_uuid,
-    TO_TIMESTAMP(kandidater.kafka_message.createdAt, 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm') AT TIME ZONE 'CET' AS hendelse_tidspunkt,
+    TO_TIMESTAMP(kandidater.kafka_message.createdAt, 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm') at TIME ZONE 'CET' as timestamp) AS hendelse_tidspunkt,
     kandidater.kafka_message.personIdentNumber as person_ident,
     DECODE(kandidater.kafka_message.kandidat, 'true', 1, 'false', 0) AS kandidat_flagg,
     kandidater.kafka_message.arsak as hendelse,
