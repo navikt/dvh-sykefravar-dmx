da0a4d6368fe91d4bede1a1f0eb6fc33ce7e4570
Aleksander Tjernes <143392502+aleksatje@users.noreply.github.com>
2024-01-17 14:02:06 +0100

    STO-3052 Initial commit - 1. Ny kilde: felles_dt_person__ident_off_id_til_fk_person1.sql 2. Lister ut fk_person1 istedenfor person_ident i fk_modia__dialogmote.sql

--- models/dvh_syfo/forkammer/modia/fk_modia__dialogmote.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__dialogmote.sql
@@ -1,14 +1,9 @@
 WITH dialogmote AS (
-  SELECT * FROM {{ source('modia', 'raw_isdialogmote') }}
-)
-
-, final AS (
-  SELECT
-    dialogmote.kafka_message.dialogmoteUuid as kilde_uuid,
+    select dialogmote.kafka_message.dialogmoteUuid as kilde_uuid,
     CAST(TO_TIMESTAMP_TZ(dialogmote.kafka_message.dialogmoteTidspunkt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') at TIME ZONE 'CET' as timestamp) AS dialogmote_tidspunkt,
     dialogmote.kafka_message.statusEndringType as hendelse,
     CAST(TO_TIMESTAMP_TZ(dialogmote.kafka_message.statusEndringTidspunkt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') at TIME ZONE 'CET' as timestamp) AS hendelse_tidspunkt,
-    dialogmote.kafka_message.personIdent as person_ident,
+    person.fk_person1 as fk_person1,
     dialogmote.kafka_message.virksomhetsnummer as virksomhetsnr,
     dialogmote.kafka_message.enhetNr as enhet_nr,
     dialogmote.kafka_message.navIdent as nav_ident,
@@ -16,13 +11,39 @@ WITH dialogmote AS (
     DECODE(dialogmote.kafka_message.arbeidstaker, 'true', 1, 'false', 0) AS arbeidstaker_flagg,
     DECODE(dialogmote.kafka_message.arbeidsgiver, 'true', 1, 'false', 0) AS arbeidsgiver_flagg,
     DECODE(dialogmote.kafka_message.sykmelder, 'true', 1, 'false', 0) AS sykmelder_flagg,
-    kafka_topic,
-    kafka_partisjon,
-    kafka_offset,
-    kafka_mottatt_dato,
-    lastet_dato,
-    kildesystem
-  FROM dialogmote dialogmote
+    dialogmote.kafka_topic,
+    dialogmote.kafka_partisjon,
+    dialogmote.kafka_offset,
+    dialogmote.kafka_mottatt_dato,
+    dialogmote.lastet_dato,
+    dialogmote.kildesystem
+  from {{ source('modia', 'raw_isdialogmote') }} as dialogmote
+  inner join {{ref('felles_dt_person__ident_off_id_til_fk_person1') }} person
+          on person.off_id = dialogmote.kafka_message.personIdent
+         and person.gyldig_til_dato = to_date('31.12.9999','DD.MM.YYYY') -- henter gyldige
+         and person.skjermet_kode not in (6, 7) -- fjerner skjerma personer
+),
+
+final as (
+  select kilde_uuid,
+         dialogmote_tidspunkt,
+         hendelse,
+         hendelse_tidspunkt,
+         fk_person1,
+         virksomhetsnr,
+         enhet_nr,
+         nav_ident,
+         tilfelle_startdato,
+         arbeidstaker_flagg,
+         arbeidsgiver_flagg,
+         sykmelder_flagg,
+         kafka_topic,
+         kafka_partisjon,
+         kafka_offset,
+         kafka_mottatt_dato,
+         lastet_dato,
+         kildesystem
+  from dialogmote
 )
 
-SELECT * FROM final
+select * from final
