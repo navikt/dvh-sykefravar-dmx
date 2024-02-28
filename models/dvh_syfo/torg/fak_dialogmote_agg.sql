ecf13c882d0a96d8082bc225ba23a153d6ed6ed8
Stig Magne Henriksen <stig.m.henriksen@gmail.com>
2024-02-28 15:30:27 +0000

    to varianter med aggregering

--- models/dvh_syfo/torg/fak_dialogmote_agg.sql
+++ models/dvh_syfo/torg/fak_dialogmote_agg.sql
@@ -2,43 +2,73 @@
     materialized='view',
 )}}
 
-wITH generell_dato AS (
-    SELECT generate_series(
-        DATE '2022-01-01',  -- Startdato
-        DATE '2022-12-31',  -- Sluttdato
-        INTERVAL '1 day'
-    )::DATE AS dato
+wITH gen_dato AS (
+    SELECT CAST((DATE '2019-01-01' + LEVEL - 1) AS TIMESTAMP) AS dato
+    FROM dual
+    CONNECT BY LEVEL <= (DATE '2024-12-31' - DATE '2019-01-01' + 1)
 ),
 fakta_gen AS (
     SELECT * FROM {{ ref('fak_dialogmote') }}
 ),
 dim_org AS (
-    SELECT * FROM {{ ref('dim_org') }}
+    SELECT * FROM {{ ref('felles_dt_p__dim_organisasjon') }}
+),
+
+dim_alder AS (
+    SELECT * FROM {{ ref('felles_dt_p__dim_alder') }}
 ),
 
 dialogmøter_agg AS (
     SELECT
-        EXTRACT(YEAR FROM g.dato) AS år,
-        EXTRACT(MONTH FROM g.dato) AS måned,
-        EXTRACT(WEEK FROM g.dato) AS uke,
-        d.enhetsnavn,
-        a.alder,
-        COUNT(*) AS antall_dialogmøter,
-        COUNT(f.date_dialogmote2) AS antall_date_dialogmote2
+        EXTRACT(YEAR FROM gen_dato.dato) AS aar,
+        EXTRACT(MONTH FROM gen_dato.dato) AS maaned,
+        TO_CHAR(gen_dato.dato, 'IW') as uke,
+        dim_org.nav_enhet_navn,
+        dim_alder.alder as alder,
+        COUNT(fakta_gen.DIALOGMOTE2_AVHOLDT_DATO) AS antall_dialogmøter2,
+        0 as antall_dialogmøter3
     FROM
-        generell_dato g
+        gen_dato
     LEFT JOIN
-        fakta_gen f ON g.dato = f.dato_dm2 OR g.dato = f.date_dialogmote2
+        fakta_gen  ON gen_dato.dato = fakta_gen.DIALOGMOTE2_AVHOLDT_DATO
     JOIN
-        dim_org d ON f.fk_dim_organisasjon = d.org_id
+        dim_org  ON fakta_gen.fk_dim_organisasjon = dim_org.pk_dim_organisasjon
+    join
+        dim_alder  ON fakta_gen.fk_dim_alder = dim_alder.pk_dim_alder
     GROUP BY
-        EXTRACT(YEAR FROM g.dato),
-        EXTRACT(MONTH FROM g.dato),
-        EXTRACT(WEEK FROM g.dato),
-        d.enhetsnavn
+         EXTRACT(YEAR FROM gen_dato.dato),
+        EXTRACT(MONTH FROM gen_dato.dato),
+        TO_CHAR(gen_dato.dato, 'IW'),
+       dim_org.nav_enhet_navn,
+       dim_alder.alder
+    union all
+     SELECT
+        EXTRACT(YEAR FROM gen_dato.dato) AS aar,
+        EXTRACT(MONTH FROM gen_dato.dato) AS maaned,
+        TO_CHAR(gen_dato.dato, 'IW') as uke,
+        dim_org.nav_enhet_navn,
+        dim_alder.alder as alder,
+        0 as antall_dialogmøter2,
+        COUNT(fakta_gen.DIALOGMOTE3_AVHOLDT_DATO) AS antall_dialogmøter3
+    FROM
+        gen_dato
+    LEFT JOIN
+        fakta_gen  ON gen_dato.dato = fakta_gen.DIALOGMOTE3_AVHOLDT_DATO
+    JOIN
+        dim_org  ON fakta_gen.fk_dim_organisasjon = dim_org.pk_dim_organisasjon
+    join
+        dim_alder  ON fakta_gen.fk_dim_alder = dim_alder.pk_dim_alder
+    GROUP BY
+         EXTRACT(YEAR FROM gen_dato.dato),
+        EXTRACT(MONTH FROM gen_dato.dato),
+        TO_CHAR(gen_dato.dato, 'IW'),
+       dim_org.nav_enhet_navn,
+       dim_alder.alder
 
 )
 SELECT * FROM dialogmøter_agg
+
+
 
 
 
---
+++ models/dvh_syfo/torg/fak_dialogmote_agg_sub.sql
@@ -0,0 +1,48 @@
+{{ config(
+    materialized='table',
+)}}
+
+wITH gen_dato AS (
+    SELECT CAST((DATE '2019-01-01' + LEVEL - 1) AS TIMESTAMP) AS dato
+    FROM dual
+    CONNECT BY LEVEL <= (DATE '2024-12-31' - DATE '2019-01-01' + 1)
+),
+fakta_gen AS (
+    SELECT * FROM {{ ref('fak_dialogmote') }}
+),
+dim_org AS (
+    SELECT * FROM {{ ref('felles_dt_p__dim_organisasjon') }}
+),
+
+dim_alder AS (
+    SELECT * FROM {{ ref('felles_dt_p__dim_alder') }}
+),
+dialogmøter_agg AS (
+SELECT
+    EXTRACT(YEAR FROM gen_dato.dato) AS aar,
+    EXTRACT(MONTH FROM gen_dato.dato) AS maaned,
+    TO_CHAR(gen_dato.dato, 'IW') as uke,
+    dim_org.nav_enhet_navn,
+    dim_alder.alder as alder,
+    COUNT(CASE WHEN fakta_gen.DIALOGMOTE2_AVHOLDT_DATO = gen_dato.dato THEN 1 END) AS antall_dialogmøter2,
+    COUNT(CASE WHEN fakta_gen.DIALOGMOTE3_AVHOLDT_DATO = gen_dato.dato THEN 1 END) AS antall_dialogmøter3
+FROM
+    gen_dato
+INNER JOIN
+    fakta_gen ON gen_dato.dato IN (fakta_gen.DIALOGMOTE2_AVHOLDT_DATO, fakta_gen.DIALOGMOTE3_AVHOLDT_DATO)
+INNER JOIN
+    dim_org ON fakta_gen.fk_dim_organisasjon = dim_org.pk_dim_organisasjon
+INNER JOIN
+    dim_alder ON fakta_gen.fk_dim_alder = dim_alder.pk_dim_alder
+GROUP BY
+    EXTRACT(YEAR FROM gen_dato.dato),
+     EXTRACT(MONTH FROM gen_dato.dato),
+     TO_CHAR(gen_dato.dato, 'IW') ,
+    dim_org.nav_enhet_navn,
+    dim_alder.alder
+)
+SELECT * FROM dialogmøter_agg
+
+
+
+
