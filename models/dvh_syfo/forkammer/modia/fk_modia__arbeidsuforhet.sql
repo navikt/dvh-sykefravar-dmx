82cb5ec99ff3bc47c15bb438406bf5816036c5dd
juliealiceskoien <julie.alice.skoien@nav.no>
2024-05-03 08:50:20 +0200

    fix: space

--- models/dvh_syfo/forkammer/modia/fk_modia__arbeidsuforhet.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__arbeidsuforhet.sql
@@ -7,17 +7,19 @@
 WITH uforhet_raw AS (
   SELECT * FROM {{ ref('fk_modia__arbeidsuforhet_raw') }}
 )
-,dvh_person_ident AS (
+
+, dvh_person_ident AS (
     SELECT * FROM {{ref('felles_dt_person__ident_off_id_til_fk_person1') }}
 )
-, uforhet_dta AS (
+
+, uforhet_data AS (
   SELECT
       JSON_VALUE(uforhet.KAFKA_MESSAGE, '$.uuid') AS kilde_uuid,
       JSON_VALUE(uforhet.KAFKA_MESSAGE, '$.personident') AS personIdent,
       CAST(TO_TIMESTAMP_TZ(JSON_VALUE(uforhet.KAFKA_MESSAGE,'$.createdAt'), 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm"Z"') at TIME ZONE 'CET' as timestamp) as createdAt,
       JSON_VALUE(uforhet.KAFKA_MESSAGE, '$.type') AS vurderingsType,
       json_value(uforhet.kafka_message,'$.veilederident') as veilederIdent,
-      json_value(uforhet.kafka_message,'$.begrunnelse') as begrunnelse,
+   --   json_value(uforhet.kafka_message,'$.begrunnelse') as begrunnelse,
     kafka_topic,
     kafka_partisjon,
     kafka_offset,
@@ -27,7 +29,8 @@ WITH uforhet_raw AS (
     kildesystem
   FROM uforhet_raw uforhet
 )
-,med_fkPerson1 as (
+
+, med_fkPerson1 as (
   SELECT
     kilde_uuid,
     dvh_person_ident.fk_person1 as fk_person1,
@@ -39,15 +42,16 @@ WITH uforhet_raw AS (
     kafka_offset,
     oppdatert_dato,
     lastet_dato,
-    kildesystem,
-    begrunnelse
-  from uforhet_dta
+    kildesystem
+  --  begrunnelse
+  from uforhet_data
   LEFT JOIN dvh_person_ident
   ON
-      uforhet_dta.personIdent = dvh_person_ident.off_id
+      uforhet_data.personIdent = dvh_person_ident.off_id
       AND dvh_person_ident.gyldig_til_dato = TO_DATE('9999-12-31', 'YYYY-MM-DD')
 )
-,final as (
+
+, final as (
   SELECT med_fkPerson1.* FROM med_fkPerson1
 )
 
