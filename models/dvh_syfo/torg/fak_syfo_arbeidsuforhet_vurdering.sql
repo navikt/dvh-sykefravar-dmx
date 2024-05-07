3e7ca4c5c1f6c7d56aec90b736ae11827e17d0de
juliealiceskoien <julie.alice.skoien@nav.no>
2024-05-07 16:30:01 +0200

    fix: oppdater spørring med største datoverdi funnet for å unngå duplikate rader (både 0 og 1 flagg settes)

--- models/dvh_syfo/torg/fak_syfo_arbeidsuforhet_vurdering.sql
+++ models/dvh_syfo/torg/fak_syfo_arbeidsuforhet_vurdering.sql
@@ -17,6 +17,9 @@ sf_hendelse_hendelsetype_39_uker as (
 ),
 
 /* Sykmeldt uten arbeidsgiver-flagg */
+/* Vil finne nærmeste korrekte rad.
+Setter radnumre basert på differansen i tid mellom uførhetsvurdering opprettet og endret kilde dato.
+Nærmeste dato / minst diff får radnummer 1. */
 sykm_uten_arbgiver_flagg as (
   select *
   from (
@@ -34,16 +37,24 @@ sykm_uten_arbgiver_flagg as (
   where rn = 1
 ),
 
---denne er ikke riktig, får 3 flere rader enn jeg skal ha
+/* 39 uker-flagg */
+/* Setter flagg dersom det finnes rad større enn tilfelle_dato (nærmeste vi kommer samme tilfelle).
+Vil fungere godt nok kort frem i tid, men gi feil resultater idet personer får flere tilfeller og
+det ikke matches med rett dato fra sf_hendelse_dag-tabellen.
+Løsning: Inkrementell daglig last for å slippe sjekke mot gamle tilfeller.  */
+
 sett_39_uker_flagg as (
     select
       uforhet.*,
-      case when (sf_hendelse_hendelsetype_39_uker.fk_dim_tid_dato_hendelse > to_number(to_char(tilfelle_startdato, 'YYYYMMDD')))
+      case when (fk_dim_tid_dato_hendelse > to_number(to_char(tilfelle_startdato, 'YYYYMMDD')))
         then 1 else 0 end as sett_39_uker_flagg
     from uforhet
-    left join sf_hendelse_hendelsetype_39_uker on
-      uforhet.fk_person1 = sf_hendelse_hendelsetype_39_uker.fk_person1
-      and uforhet.tilfelle_startdato between sf_hendelse_hendelsetype_39_uker.gyldig_fra_dato and sf_hendelse_hendelsetype_39_uker.gyldig_til_dato
+    left join
+      (select fk_person1, max(fk_dim_tid_dato_hendelse) as fk_dim_tid_dato_hendelse
+      from sf_hendelse_hendelsetype_39_uker
+      group by fk_person1
+      ) siste_hendelse on
+      uforhet.fk_person1 = siste_hendelse.fk_person1
 ),
 
 tildelt_enhet_tilfelle_startdato as (
