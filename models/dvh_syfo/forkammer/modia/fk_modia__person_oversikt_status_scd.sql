56ba6d49b947fb50bb02a31d9f2104f5553fd9dc
juliealiceskoien <julie.alice.skoien@nav.no>
2024-05-15 14:49:31 +0200

    Løst inkrementell last. Brukes med full refresh eller daglig oppdatering

--- models/dvh_syfo/forkammer/modia/fk_modia__person_oversikt_status_scd.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__person_oversikt_status_scd.sql
@@ -1,35 +1,73 @@
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='incremental',
+        unique_key=['fk_person1', 'tildelt_enhet', 'lastet_dato'],
+        incremental_strategy='merge'
+    )
+}}
 
-with person_oversikt_status_snapshot as (
-  select * from {{ source('modia', 'fk_syfo_person_oversikt_status__snapshot') }}
-  where trunc(lastet_dato) != to_date('14.05.2024', 'dd.mm.yyyy') AND trunc(lastet_dato) != to_date('15.05.2024', 'dd.mm.yyyy') -- for stopp lasting av gårsdagens data
-  -- slik at vi kan teste last med ny dags data
+
+with person_oversikt_status as (
+  select
+    uuid,
+    fk_person1,
+    tildelt_enhet,
+    kilde_opprettet_dato,
+    kilde_sist_endret_dato,
+    tildelt_enhet_updated_at,
+    motestatus,
+    kildesystem,
+    lastet_dato,
+    oppdatert_dato as oppdatert_dato_old, --må fjernes fra kildeinnlasting i DAG syfo_kandidater_enhet
+    dbt_scd_id,
+    dbt_updated_at,
+    dbt_valid_from,
+    dbt_valid_to
+  from {{ source('modia', 'fk_syfo_person_oversikt_status__snapshot')}}
+--from {{ ref('fk_modia__person_oversikt_status_scd_test')}} -- bruk ved test
 ),
 
 nyeste_rader as (
-  select * from person_oversikt_status_snapshot
---  where trunc(lastet_dato) > (select max(oppdatert_dato_dbt) from person_oversikt_status_scd)
+  select * from person_oversikt_status
+
+{% if is_incremental() %}
+
+  where trunc(lastet_dato) > (select max(oppdatert_dato) from {{ this }})
+
+{% endif  %}
+
 ),
 
-rader_til_operasjon as (
-    select * from person_oversikt_status_snapshot
-    where fk_person1 in (select fk_person1 from nyeste_rader) --and dbt_valid_to is null for å bare ta siste?
+rader_til_oppdatering as (
+    select *
+    from (
+        select
+            person_oversikt_status.*,
+            row_number() over ( partition by fk_person1 order by tildelt_enhet_updated_at desc) as radnr
+        from person_oversikt_status)
+
+{% if is_incremental() %}
+
+    where fk_person1 in (select fk_person1 from nyeste_rader) and radnr in (1,2) -- Trenger bare nest siste og siste ved oppdatering av gyldighetsintervall
+
+{% endif  %}
+
 ),
 
 finn_neste_dato_for_tildelt_enhet as (
     select
-       person_oversikt_status_snapshot.*,
+       rader_til_oppdatering.*,
         lead (tildelt_enhet_updated_at, 1) over ( partition by fk_person1 order by tildelt_enhet_updated_at  ) as neste_dato
-    from person_oversikt_status_snapshot
+    from rader_til_oppdatering
 ),
 
 sett_gyldig_kolonner as (
     select
         finn_neste_dato_for_tildelt_enhet.*,
         trunc(tildelt_enhet_updated_at) as gyldig_fra_dato,
-        case when neste_dato is null then trunc(to_date('9999-12-31', 'YYYY-MM-DD')) else neste_dato - 1 end as gyldig_til_dato,
+        case when neste_dato is null then trunc(to_date('9999-12-31', 'YYYY-MM-DD')) else trunc(neste_dato - 1) end as gyldig_til_dato,
         case when dbt_valid_to is null then 1 else 0 end as gyldig_flagg,
-        trunc(sysdate-2) as oppdatert_dato_dbt --finn nytt navn for å ikke ligne DBT_UPDATED_AT.
+        trunc(sysdate) as oppdatert_dato --endre til systdate-1 ved test
     from finn_neste_dato_for_tildelt_enhet
 
 ),
@@ -45,15 +83,10 @@ final as (
     motestatus,
     kildesystem,
     lastet_dato,
-    OPPDATERT_DATO,--vurder dropp
-    DBT_SCD_ID,--vurder dropp
-    DBT_UPDATED_AT,--vurder dropp
-    DBT_VALID_FROM,--vurder dropp
-    DBT_VALID_TO,--vurder dropp
+    oppdatert_dato,
     gyldig_fra_dato,
     gyldig_til_dato,
-    gyldig_flagg,
-    oppdatert_dato_dbt
+    gyldig_flagg
 
   from sett_gyldig_kolonner
 )
