ad5e94cd86722f84c3382f9acc94dfbed37689e2
juliealiceskoien <julie.alice.skoien@nav.no>
2024-05-27 15:25:11 +0200

    add: full flyt for å pakke ut oppfolgingstilfelle

--- models/dvh_syfo/forkammer/modia/fk_modia__oppfolgingstilfelle.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__oppfolgingstilfelle.sql
@@ -1,61 +1,97 @@
-{{
-  config(
-    materialized = 'view',
-  )
-}}
+with src_oppfolgingstilfelle_person as (
+  select
+      kafka_hash
+      , kafka_topic
+      , kafka_partisjon
+      , kafka_key
+      , kafka_offset
+      , kafka_mottatt_dato
+      , kafka_timestamp
+      , kafka_message
+      , lastet_dato
+      , oppdatert_dato
+      , kildesystem
+   from {{ source('modia', 'raw_oppfolgingstilfelle') }}
+   where kafka_offset > 17000000
+)
 
-WITH oppfolgingstilfelle AS (
-  SELECT * FROM {{ ref('fk_modia__oppfolgingstilfelle_raw') }}
+, dvh_person_ident as (
+    select  * from {{ref('felles_dt_person__ident_off_id_til_fk_person1') }}
 )
-,dvh_person_ident AS (
-    SELECT * FROM {{ref('felles_dt_person__ident_off_id_til_fk_person1') }}
+
+, json_data_person as (
+    select
+      json_value(oppf.kafka_message, '$.uuid') as uuid
+      , json_value(oppf.kafka_message, '$.personIdentNumber') as person_ident
+      , json_query(oppf.kafka_message, '$.oppfolgingstilfelleList') as oppfolgingstilfelle_list
+      , kafka_partisjon
+      , kafka_offset
+      , kafka_timestamp
+      , kafka_mottatt_dato
+      , lastet_dato
+      , oppdatert_dato
+      , kildesystem
+  from src_oppfolgingstilfelle_person oppf
 )
-, aktivitet_dta AS (
-  SELECT
-      JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE, '$.uuid') AS kilde_uuid,
-      JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE, '$.personIdentNumber') AS personIdent,
-      CAST(TO_TIMESTAMP_TZ(JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE, '$.stoppunktAt'), 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') at TIME ZONE 'CET' as timestamp) AS stoppunktAt,
-       CAST(TO_TIMESTAMP_TZ(JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE,'$.createdAt'), 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm"Z"') at TIME ZONE 'CET' as timestamp) as createdAt,
-        JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE, '$.oppfolgingstilfelleList[4].antallSykedager') as antallSykedager,
-        JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE, '$.oppfolgingstilfelleList[0].gradertAtTilfelleEnd') as gradertTifelle_End,
-        JSON_VALUE(oppfolgingstilfelle.KAFKA_MESSAGE, '$.oppfolgingstilfelleList[1].arbeidstakerAtTilfelleEnd') as arbeidstakerTifelle_End,
-        CAST(TO_TIMESTAMP_TZ(JSON_VALUE(oppfolgingstilfelle.kafka_message,'$.oppfolgingstilfelleList[2].start'), 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm"Z"') at TIME ZONE 'CET' as timestamp) AS tilfelle_start_dato,
-        CAST(TO_TIMESTAMP_TZ(JSON_VALUE(oppfolgingstilfelle.kafka_message,'$.oppfolgingstilfelleList[3].end'), 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm"Z"') at TIME ZONE 'CET' as timestamp) AS tilfelle_stopp_dato,
-    kafka_topic,
-    kafka_partisjon,
-    kafka_offset,
-    kafka_mottatt_dato,
-    oppdatert_dato,
-    lastet_dato,
-    kildesystem
-  FROM oppfolgingstilfelle oppfolgingstilfelle
+
+, json_data_oppfolgingstilfeller as (
+  select
+      oppf_person.*
+      , case when jt.gradert_at_tilfelle_end = 'true' then 1 else 0 end as gradert_at_tilfelle_end_flagg
+      , case when jt.arbeidstaker_at_tilfelle_end = 'true' then 1 else 0 end as arbeidstaker_at_tilfelle_end_flagg
+      , jt.syfo_start_dato as syfo_start_dato
+      , jt.syfo_slutt_dato as syfo_slutt_dato
+      , jt.antall_sykedager as antall_sykedager
+      , jt.virksomhetsnummer_list as virksomhetsnummer_list
+    from json_data_person oppf_person
+    inner join json_table(
+        oppf_person.oppfolgingstilfelle_list format json
+        , '$[*]' columns(
+            gradert_at_tilfelle_end varchar2(255) path '$.gradertAtTilfelleEnd'
+            , arbeidstaker_at_tilfelle_end varchar2(255) path '$.arbeidstakerAtTilfelleEnd'
+            , syfo_start_dato date path '$.start'
+            , syfo_slutt_dato date path '$.end'
+            , antall_sykedager number(38) path '$.antallSykedager'
+            , virksomhetsnummer_list varchar2(255) format json path '$.virksomhetsnummerList'
+        )
+    ) jt on 1 = 1
 )
-,med_fkPerson1 as (
-  SELECT
-    kilde_uuid,
-    createdAt,
-    antallSykedager,
-    gradertTifelle_End,
-    arbeidstakerTifelle_End,
-    tilfelle_start_dato,
-    tilfelle_stopp_dato,
-    kafka_topic,
-    kafka_partisjon,
-    kafka_offset,
-    kafka_mottatt_dato,
-    oppdatert_dato,
-    lastet_dato,
-    kildesystem,
-    dvh_person_ident.fk_person1 as fk_person1
-  from aktivitet_dta
-  LEFT JOIN dvh_person_ident
-  ON
-      aktivitet_dta.personIdent = dvh_person_ident.off_id
-      AND dvh_person_ident.gyldig_til_dato = TO_DATE('9999-12-31', 'YYYY-MM-DD')
+
+, med_fk_person1 as (
+  select
+    oppf.*
+    , dvh_person_ident.fk_person1 as fk_person1
+  from json_data_oppfolgingstilfeller oppf
+  left join dvh_person_ident on
+      oppf.person_ident = dvh_person_ident.off_id
+      and oppf.syfo_start_dato between dvh_person_ident.gyldig_fra_dato and dvh_person_ident.gyldig_til_dato
 )
-,final as (
-SELECT med_fkPerson1.* FROM med_fkPerson1
+
+, siste_historikk as (
+  select
+    a.*
+  from med_fk_person1 a
+  where kafka_timestamp = (select max(b.kafka_timestamp) from med_fk_person1 b where a.fk_person1 = b.fk_person1)
+)
+
+, final as (
+  select
+    uuid
+    , fk_person1
+    , gradert_at_tilfelle_end_flagg
+    , arbeidstaker_at_tilfelle_end_flagg
+    , syfo_start_dato
+    , syfo_slutt_dato
+    , antall_sykedager
+    , virksomhetsnummer_list
+    , kafka_partisjon
+    , kafka_offset
+    , kafka_timestamp
+    , kafka_mottatt_dato
+    , lastet_dato
+    , oppdatert_dato
+    , kildesystem
+  from siste_historikk
 )
 
 select * from final
-
