42c73ae06d29d496caa5e183270b90225cbf5994
juliealiceskoien <julie.alice.skoien@nav.no>
2024-06-17 15:43:02 +0200

    add: view for sektor

---
+++ models/syfra/agg_ia_sykefravar__sektor.sql
@@ -0,0 +1,45 @@
+with siste_periode as (
+  select max(b.rapport_periode) periode
+  from {{ source('dt_kodeverk', 'dim_versjon') }}  b
+  join {{ source('dt_kodeverk', 'publiseringstabell') }}  p on
+      p.rapport_periode = b.rapport_periode and
+      p.tabell_navn = 'IA_PARAM' and
+      p.offentlig_dato < sysdate and
+      b.tabell_navn = 'FAK_IA_SYKEFRAVAR'
+),
+
+sykefravar_statistikk_sektor as (
+  select
+    sektor,
+    arstall,
+    kvartal,
+    round( sum(taptedv)/sum(muligedv) * 100, 1) as prosent,
+    sum(taptedv) taptedv,
+    sum(muligedv) muligedv,
+    sum(antpers) antpers
+from {{ source('dt_p', 'fak_ia_sykefravar') }} fak
+join {{ source('dt_kodeverk', 'dim_versjon') }} dim on
+    dim.pk_dim_versjon = fak.fk_dim_versjon and
+    dim.tabell_navn = 'FAK_IA_SYKEFRAVAR'
+    and dim.offentlig_flagg = 1
+where dim.rapport_periode <= (select periode from siste_periode)
+and dim.rapport_periode > (select periode - 500 from siste_periode)
+group by
+    sektor,
+    arstall,
+    kvartal
+),
+
+final as (
+  select
+    sektor,
+    arstall,
+    kvartal,
+    prosent,
+    taptedv,
+    muligedv,
+    antpers
+  from sykefravar_statistikk_sektor
+)
+
+select * from final
