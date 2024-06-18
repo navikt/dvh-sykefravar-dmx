366ae7f07a62c2bc25413da5742e3dbea3e90605
juliealiceskoien <julie.alice.skoien@nav.no>
2024-06-18 12:58:05 +0200

    add: view for virksomhet_metadata

---
+++ models/syfra/agg_ia_sykefravar__virksomhet_metadata.sql
@@ -0,0 +1,30 @@
+with siste_periode as (
+  select max(b.rapport_periode) periode
+  from {{ source('dt_kodeverk', 'dim_versjon') }}  b
+  join {{ source('dt_kodeverk', 'publiseringstabell') }}  p on
+      p.rapport_periode = b.rapport_periode and
+      p.tabell_navn = 'IA_PARAM' and
+      p.offentlig_dato < sysdate and
+      b.tabell_navn = 'FAK_IA_SYKEFRAVAR'
+),
+
+sykefravar_statistikk_virksomhet_metadata as (
+    select
+    orgnr,
+    arstall,
+    kvartal,
+    sektor,
+    --primærnæring
+    naring_primar_kode primærnæringskode,
+    rectype
+from {{ source('dt_p', 'fak_ia_sykefravar') }} fak
+join {{ source('dt_kodeverk', 'dim_versjon') }} dim on
+    dim.pk_dim_versjon = fak.fk_dim_versjon and
+    dim.tabell_navn = 'FAK_IA_SYKEFRAVAR'
+    and dim.offentlig_flagg = 1
+where dim.rapport_periode <= (select periode from siste_periode)
+and dim.rapport_periode > (select periode - 500 from siste_periode)
+)
+
+
+select * from sykefravar_statistikk_virksomhet_metadata
