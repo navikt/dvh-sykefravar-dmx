bb819e8e34c550e28077fef572ea9a6bcc99f7e1
juliealiceskoien <julie.alice.skoien@nav.no>
2024-08-07 13:23:00 +0200

    rename: view før utvidelse med varighet

---
+++ models/syfra/agg_ia_sykefravar/agg_ia_sykefravar__naering_old.sql
@@ -0,0 +1,53 @@
+/********************************************************
+View til Team PIA
+*********************************************************/
+{{ config(
+    post_hook= ["grant READ ON {{this}} to DVH_SYK_DBT"]
+)}}
+
+with siste_periode as (
+  select max(b.rapport_periode) periode
+  from {{ source('dt_kodeverk', 'dim_versjon') }}  b
+  join {{ source('dt_kodeverk', 'publiseringstabell') }}  p on
+      p.rapport_periode = b.rapport_periode and
+      p.tabell_navn = 'IA_PARAM' and
+      p.offentlig_dato < sysdate and
+      b.tabell_navn = 'FAK_IA_SYKEFRAVAR'
+),
+
+sykefravar_statistikk_naering as (
+  select
+    naring,
+    arstall,
+    kvartal,
+    sum(taptedv) taptedv,
+    sum(muligedv) muligedv,
+    sum(antpers) antpers
+from {{ source('syfra', 'fak_ia_sykefravar') }} fak
+join {{ source('dt_kodeverk', 'dim_versjon') }} dim on
+    dim.pk_dim_versjon = fak.fk_dim_versjon and
+    dim.tabell_navn = 'FAK_IA_SYKEFRAVAR'
+    and dim.offentlig_flagg = 1
+where dim.rapport_periode <= (select periode from siste_periode)
+and dim.rapport_periode > (select periode - 500 from siste_periode)
+-- Filtrerer for kun rectype 2: VIRKSOMHET (B-nummer), tilsvarende offisiell sykefraværsstatistikk
+and fak.rectype = 2
+group by
+    naring,
+    arstall,
+    kvartal
+),
+
+final as (
+  select
+    naring,
+    arstall,
+    kvartal,
+    round(taptedv/muligedv * 100, 1) as prosent,
+    taptedv,
+    muligedv,
+    antpers
+  from sykefravar_statistikk_naering
+)
+
+select * from final
