3e5e48c83bae3a8d20a83b9829f6054c7a4eb9a0
Elisabeth Nes <49159822+elisnes@users.noreply.github.com>
2024-09-12 13:36:46 +0200

    Utpakking av SYKP-utbetalingar

---
+++ models/sykp/staging/stg_sykp__arbeidsgiver_oppdrag.sql
@@ -0,0 +1,58 @@
+ /* Daglig last av utbetalingsoppdrag for sykepengar, der utbetalingen skjer til arbeidsgivar.
+-- Kun events av type utbetaling_utbetalt tas med.
+-- Nye rader legges til med 'append'.
+-- Kjøres med flagg --full-refresh for å opprette tabellen på nytt. */
+
+{{ config(
+    materialized='table'
+)}}
+
+with src_arbeidsgiver_oppdrag as (
+  select
+    JSON_VALUE(utbetaling.KAFKA_MESSAGE, '$.utbetalingId') as utbetaling_id,
+    JSON_VALUE(utbetaling.kafka_message,'$.arbeidsgiverOppdrag.mottaker') as mottaker_orgnummer,
+    json_value(utbetaling.kafka_message,'$.arbeidsgiverOppdrag.fagsystemId') as fagsystem_id,
+    json_value(utbetaling.kafka_message,'$.arbeidsgiverOppdrag.fagområde') as fagomraade,
+    json_query(utbetaling.kafka_message, '$.arbeidsgiverOppdrag.utbetalingslinjer') as utbetalingslinjer,
+    jt_linjer.totalbelop as totalbelop,
+    jt_linjer.stonadsdager as stonadsdager,
+    jt_linjer.grad as grad,
+    jt_linjer.dagsats as dagsats,
+    jt_linjer.utbetalt_fom_dato as utbetalt_fom_dato,
+    jt_linjer.utbetalt_tom_dato as utbetalt_tom_dato,
+    utbetaling.kildesystem,
+    utbetaling.lastet_dato as oppdatert_dato,
+    utbetaling.lastet_dato
+  from {{ source('sykp', 'raw_utbetaling') }} utbetaling
+    CROSS JOIN JSON_TABLE(utbetaling.kafka_message, '$.arbeidsgiverOppdrag.utbetalingslinjer[*]'
+          COLUMNS (totalbelop        NUMBER(10,0) PATH '$.totalbelÃžp',
+                   stonadsdager      NUMBER(3,0)  PATH '$.stÃžnadsdager',
+                   dagsats           NUMBER(10,0) PATH '$.dagsats',
+                   utbetalt_fom_dato DATE PATH '$.fom',
+                   utbetalt_tom_dato DATE PATH '$.tom',
+                   grad NUMBER(3,0)  PATH '$.grad')) jt_linjer
+    where kafka_mottatt_dato < trunc(sysdate)
+    and json_value(utbetaling.kafka_message,'$.event') = 'utbetaling_utbetalt'
+  ),
+
+  final as (
+    select
+      cast( utbetaling_id as varchar2(100 char) ) as utbetaling_id,
+      cast( mottaker_orgnummer as varchar2(9 char) ) as mottaker_orgnummer,
+      cast( fagsystem_id as varchar2(50 char) ) as fagsystem_id,
+      cast( fagomraade as varchar2(50 char) ) as fagomraade,
+      cast( utbetalingslinjer as varchar2(1000 char) ) as utbetalingslinjer,
+      cast( totalbelop as number(10,0) ) as totalbelop,
+      cast( stonadsdager as number(3,0) ) as stonadsdager,
+      cast( grad as number(3,0) ) as grad,
+      cast( dagsats as number(10,0) ) as dagsats,
+      cast( utbetalt_fom_dato as date) as utbetalt_fom_dato,
+      cast( utbetalt_tom_dato as date) as utbetalt_tom_dato,
+      cast( lastet_dato as date ) as lastet_dato,
+      cast( oppdatert_dato as date ) as oppdatert_dato,
+      cast( kildesystem as varchar2(10) ) as kildesystem
+  from  src_arbeidsgiver_oppdrag
+  )
+  select * from final
+
+
--- models/sykp/staging/stg_sykp__vedtak.sql
+++ models/sykp/staging/stg_sykp__vedtak.sql
@@ -1,3 +1,7 @@
+/* Daglig last av sykepengevedtak.
+-- Nye rader legges til med 'append'.
+-- Kjøres med flagg --full-refresh for å opprette tabellen på nytt. */
+
 with src_vedtak as (
   select
     json_value(vedtak.kafka_message,'$.fødselsnummer') as fnr,
@@ -5,8 +9,6 @@ with src_vedtak as (
     CAST(TO_TIMESTAMP_TZ(JSON_VALUE(vedtak.kafka_message,'$.fom'), 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AS timestamp) AS soknad_fom_dato,
     CAST(TO_TIMESTAMP_TZ(JSON_VALUE(vedtak.kafka_message,'$.tom'), 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') AS timestamp) AS soknad_tom_dato,
     JSON_VALUE(vedtak.KAFKA_MESSAGE, '$.utbetalingId') as utbetaling_id,
-    substr(json_query(vedtak.kafka_message, '$.dokumenter'), instr(json_query(vedtak.kafka_message, '$.dokumenter'), 'Søknad') - 46, 36) as old_soknad_id,
-    substr(json_query(vedtak.kafka_message, '$.dokumenter'), instr(json_query(vedtak.kafka_message, '$.dokumenter'), 'Sykmelding') - 46, 36) as old_sykmelding_id,
     json_query(vedtak.KAFKA_MESSAGE, '$.dokumenter') as dokumenter,
     vedtak.kafka_topic,
     vedtak.kafka_partisjon,
@@ -15,7 +17,20 @@ with src_vedtak as (
     vedtak.kildesystem,
     sysdate as oppdatert_dato,
     sysdate as lastet_dato
-  from {{ source('dvh_sykp', 'raw_vedtak') }} vedtak
+  from {{ source('sykp', 'raw_vedtak') }} vedtak
+  where kafka_mottatt_dato < trunc(sysdate)
+  and nvl(json_value(vedtak.kafka_message,'$.event'), 'vedtak_ok') <> 'vedtak_annullert'
+
+  {% if is_incremental() %}
+    and
+        not exists (
+        select 1
+        from {{ this }} old
+        where vedtak.kafka_partisjon = old.kafka_partisjon
+          and vedtak.kafka_offset = old.kafka_offset -- legger til rad dersom kombinasjonen av partisjon og offset ikke allerede finnes i datasettet
+      )
+  {% endif %}
+
   ),
 
   dvh_person_ident as (
@@ -27,7 +42,7 @@ with src_vedtak as (
    from  {{ ref('felles_dt_person__ident_off_id_til_fk_person1') }}
 ),
 
-vedtak_med fk_person1 as (
+vedtak_med_fk_person1 as (
   select
     vedtak.*,
     person.fk_person1 as pasient_fk_person1
@@ -38,11 +53,12 @@ vedtak_med fk_person1 as (
     and person.skjermet_kode not in (6, 7)
 ),
 
+-- Hentar ut alle dokument, både sykmeldingar, søknader og inntektsmeldingar
 dokument as (
     select
         f.pasient_fk_person1,
-        f.soknad_fom_dato,
-        f.soknad_tom_dato,
+        f.kafka_partisjon,
+        f.kafka_offset,
         jt.dokument_id as dokument_id,
         jt.vedtak_type as vedtak_type
     from vedtak_med_fk_person1 f
@@ -55,34 +71,33 @@ dokument as (
     ) jt on 1 = 1
 ), -- select dokument.* from dokument where vedtak_type in ('Sykmelding', 'Søknad');
 
+-- Legg til alle sykmelding_id'ar og søknads-id'ar som finst på eit vedtak
 stg_vedtak as (
   select
-    v.*
+    v.*,
     soknad.dokument_id as soknad_id,
     sykmelding.dokument_id as sykmelding_id
   from vedtak_med_fk_person1 v
   left outer join dokument soknad
     on v.pasient_fk_person1 = soknad.pasient_fk_person1
-    and v.soknad_fom_dato = soknad.soknad_fom_dato
-    and v.soknad_tom_dato = soknad.soknad_tom_dato
+    and v.kafka_partisjon = soknad.kafka_partisjon
+    and v.kafka_offset = soknad.kafka_offset
     and soknad.vedtak_type = 'Søknad'
   left outer join dokument sykmelding
     on v.pasient_fk_person1 = sykmelding.pasient_fk_person1
-    and v.soknad_fom_dato = sykmelding.soknad_fom_dato
-    and v.soknad_tom_dato = sykmelding.soknad_tom_dato
+    and v.kafka_partisjon = sykmelding.kafka_partisjon
+    and v.kafka_offset = sykmelding.kafka_offset
     and sykmelding.vedtak_type = 'Sykmelding'
 ),   -- select * from stg_vedtak
 
-final as (
+pre_final as (
   select
     cast( pasient_fk_person1 as varchar2(100) ) as pasient_fk_person1,
     cast( organisasjonsnummer as varchar2(100) ) as organisasjonsnummer,
     cast( soknad_fom_dato as date ) as soknad_fom_dato,
     cast( soknad_tom_dato as date ) as soknad_tom_dato,
     cast( soknad_id as varchar2(100) ) as soknad_id,
-    cast( old_soknad_id as varchar2(100) ) as old_soknad_id,
     cast( sykmelding_id as varchar2(100) ) as sykmelding_id,
-    cast( old_sykmelding_id as varchar2(100) ) as old_sykmelding_id,
     cast( utbetaling_id as varchar2(100) ) as utbetaling_id,
     cast( kafka_topic as varchar2(100) ) as kafka_topic,
     cast( kafka_partisjon as number ) as kafka_partisjon,
@@ -92,5 +107,38 @@ final as (
     cast( oppdatert_dato as date ) as oppdatert_dato,
     cast( kildesystem as varchar2(10) ) as kildesystem
   from stg_vedtak
-  ) select * from final;
+  ),  -- select * from pre_final
+
+-- Begranser antall rader ved å kun velge den med min(sykmelding_id) og min(søknad_id.
+-- Vi vil ha berre ei rad per kafka-melding, og plukkar første sykmelding_id og søknad_id fra meldingen)
+  final as (
+    select
+    pasient_fk_person1,
+    organisasjonsnummer,
+    soknad_fom_dato,
+    soknad_tom_dato,
+    min(soknad_id) as soknad_id,
+    min(sykmelding_id) as sykmelding_id,
+    utbetaling_id,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    oppdatert_dato,
+    kildesystem
+  from pre_final
+  group by     pasient_fk_person1,
+    organisasjonsnummer,
+    soknad_fom_dato,
+    soknad_tom_dato,
+    utbetaling_id,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    lastet_dato,
+    oppdatert_dato,
+    kildesystem
+  ) select * from final
 
