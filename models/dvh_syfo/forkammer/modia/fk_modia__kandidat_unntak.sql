dd0f01c12b070537517d0a06bb8ce81f360423de
Elisabeth Nes <49159822+elisnes@users.noreply.github.com>
2025-03-03 16:15:12 +0100

    endra til outer join i fk_modia__kandidat_unntak, la fk_modia__kandidat_unntak inn i modia_models.yml.

--- models/dvh_syfo/forkammer/modia/fk_modia__kandidat_unntak.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__kandidat_unntak.sql
@@ -1,5 +1,5 @@
 WITH kandidater AS (
-  SELECT * FROM {{ ref('modia', 'fk_modia__kandidat') }}
+  SELECT * FROM {{ ref('fk_modia__kandidat') }}
 ),
 
 final as (
@@ -8,19 +8,20 @@ final as (
     k.hendelse_tidspunkt,
     k.fk_person1,
     k.kandidat_flagg,
-    k.hendelseunntakarsak,
+    k.hendelse,
+    k.unntakarsak,
     k.tilfelle_startdato,
-    nvl(u.nav_ident, k.nav_ident) as nav_ident,
-    kafka_topic,
-    kafka_partisjon,
-    kafka_offset,
-    kafka_mottatt_dato,
-    lastet_dato,
-    kildesystem
+    nvl(u.created_by, k.nav_ident) as nav_ident,
+    k.kafka_topic,
+    k.kafka_partisjon,
+    k.kafka_offset,
+    k.kafka_mottatt_dato,
+    k.lastet_dato,
+    k.kildesystem
   from kandidater k
-  join {{ source('modia', 'fk_dm_unntak_historikk_fra_2023') }} u
+  left outer join {{ source('modia', 'fk_dm_unntak_historikk_fra_2023') }} u
     on k.fk_person1 = u.fk_person1
     and trunc(k.hendelse_tidspunkt) = u.created_at_dato
-),
+)
 
 select * from final
