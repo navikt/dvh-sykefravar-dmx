d84243071c92182a29179d80d545527ceb753801
Julie Alice Skøien <julie.alice.skoien@nav.no>
2026-05-07 08:11:41 +0000

    Endrer fra timestamps til date og måten vi filtrerer ut kode 6/7-personer på

--- models/dvh_syfo/forkammer/modia/fk_modia__dialogmote.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__dialogmote.sql
@@ -4,14 +4,14 @@
 
 WITH dialogmote AS (
     select dialogmote.kafka_message.dialogmoteUuid as kilde_uuid,
-    CAST(TO_TIMESTAMP_TZ(dialogmote.kafka_message.dialogmoteTidspunkt, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') at TIME ZONE 'CET' as timestamp) AS dialogmote_tidspunkt,
+    TO_TIMESTAMP_TZ(dialogmote.kafka_message.dialogmoteTidspunkt, 'YYYY-MM-DD HH24:MI:SS.ff6+TZH:TZM') at TIME ZONE 'Europe/Oslo' AS dialogmote_tidspunkt,
     dialogmote.kafka_message.statusEndringType as hendelse,
-    CAST(TO_TIMESTAMP_TZ(dialogmote.kafka_message.statusEndringTidspunkt, 'YYYY-MM-DD HH24:MI:SS.FF:TZH:TZM') at TIME ZONE 'CET' as timestamp) AS hendelse_tidspunkt,
+    TO_TIMESTAMP_TZ(dialogmote.kafka_message.statusEndringTidspunkt, 'YYYY-MM-DD HH24:MI:SS.ff6+TZH:TZM') at TIME ZONE 'Europe/Oslo' AS hendelse_tidspunkt,
     person.fk_person1 as fk_person1,
     dialogmote.kafka_message.virksomhetsnummer as virksomhetsnr,
     dialogmote.kafka_message.enhetNr as enhet_nr,
     dialogmote.kafka_message.navIdent as nav_ident,
-    CAST(TO_TIMESTAMP_TZ(dialogmote.kafka_message.tilfelleStartdato, 'YYYY-MM-DD HH24:MI:SS:TZH:TZM') at TIME ZONE 'CET' as timestamp) AS tilfelle_startdato,
+    TO_TIMESTAMP_TZ(dialogmote.kafka_message.tilfelleStartdato, 'YYYY-MM-DD HH24:MI:SS.ff6+TZH:TZM') at TIME ZONE 'Europe/Oslo' AS tilfelle_startdato,
     DECODE(dialogmote.kafka_message.arbeidstaker, 'true', 1, 'false', 0) AS arbeidstaker_flagg,
     DECODE(dialogmote.kafka_message.arbeidsgiver, 'true', 1, 'false', 0) AS arbeidsgiver_flagg,
     DECODE(dialogmote.kafka_message.sykmelder, 'true', 1, 'false', 0) AS sykmelder_flagg,
@@ -24,15 +24,16 @@ WITH dialogmote AS (
   from {{ source('modia', 'raw_isdialogmote') }} dialogmote
   inner join {{ ref('felles_dt_person__ident_off_id_til_fk_person1') }} person
           on person.off_id = dialogmote.kafka_message.personIdent
-         and person.gyldig_til_dato = to_date('31.12.9999','DD.MM.YYYY') -- henter gyldige
+         --and person.gyldig_til_dato = to_date('31.12.9999','DD.MM.YYYY') -- henter gyldige
+         and dialogmote.kafka_mottatt_dato between person.gyldig_fra_dato and person.gyldig_til_dato
          and person.skjermet_kode not in (6, 7) -- fjerner skjerma personer
 ),
 
 final as (
   select kilde_uuid,
-         dialogmote_tidspunkt,
+         cast(dialogmote_tidspunkt as date) as dialogmote_tidspunkt,
          hendelse,
-         hendelse_tidspunkt,
+         cast(hendelse_tidspunkt as date) as hendelse_tidspunkt,
          fk_person1,
          virksomhetsnr,
          enhet_nr,
--- models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
+++ models/dvh_syfo/forkammer/modia/fk_modia__kandidat.sql
@@ -5,10 +5,10 @@
 WITH kandidater AS (
   SELECT * FROM {{ source('modia', 'raw_isdialogmotekandidat') }}
 )
-, final AS (
+, utpakking AS (
   SELECT
     kandidater.kafka_message.uuid as kilde_uuid,
-    CAST(TO_TIMESTAMP(kandidater.kafka_message.createdAt, 'yyyy-mm-dd"T"hh24:mi:ss.fftzh:tzm') at TIME ZONE 'CET' as timestamp) AS hendelse_tidspunkt,
+    TO_TIMESTAMP_TZ(kandidater.kafka_message.createdAt, 'YYYY-MM-DD"T"HH24:MI:SS.FFTZR') at TIME ZONE 'Europe/Oslo' AS hendelse_tidspunkt,
     person.fk_person1 as fk_person1,
     DECODE(kandidater.kafka_message.kandidat, 'true', 1, 'false', 0) AS kandidat_flagg,
     kandidater.kafka_message.arsak as hendelse,
@@ -24,7 +24,28 @@ WITH kandidater AS (
   FROM kandidater kandidater
   inner join {{ ref('felles_dt_person__ident_off_id_til_fk_person1') }} person
           on person.off_id = kandidater.kafka_message.personIdentNumber
-         and person.gyldig_til_dato = to_date('31.12.9999','DD.MM.YYYY') -- henter gyldige
+         --and person.gyldig_til_dato = to_date('31.12.9999','DD.MM.YYYY') -- henter gyldige
+         and kandidater.kafka_mottatt_dato between person.gyldig_fra_dato and person.gyldig_til_dato
          and person.skjermet_kode not in (6, 7) -- fjerner skjerma personer
+),
+
+final as (
+  select
+    kilde_uuid,
+    cast(hendelse_tidspunkt as date) as hendelse_tidspunkt,
+    fk_person1,
+    kandidat_flagg,
+    hendelse,
+    unntakarsak,
+    tilfelle_startdato,
+    nav_ident,
+    kafka_topic,
+    kafka_partisjon,
+    kafka_offset,
+    kafka_mottatt_dato,
+    sysdate as lastet_dato,
+    sysdate as oppdatert_dato,
+    kildesystem
+  from utpakking
 )
 SELECT * FROM final
